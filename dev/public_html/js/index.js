d761b2115ce7f0f34a23796874cf1962aa4c8fab
mads-jakobsen <mads.jakobsen@gmail.com>
2016-06-23 23:40:44 +0200

    Improved UX/Implemented imagegrab in main image processing
    Former-commit-id: 9fd359170cc072ac3e0485d0364bf50a809e16b8

--- dev/public_html/js/index.js
+++ dev/public_html/js/index.js
@@ -3,16 +3,34 @@
 
 // Bug Hunters
 $(document).ready(function () {
-console.log('Find any bugs or security issues? Contact us on https://discord.gg/8mCNvKp. We greatly appreciate your help :)')
+    console.log('Find any bugs or security issues? Contact us on https://discord.gg/8mCNvKp. We greatly appreciate your help :)')
 })
 
 
+
+
+// Show/Hide Menu Bar when appropriate
+var lastScrollTop = 0;
+
+$(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+        $('.top-nav').css('transform', 'translateY(-90px)')
+    } else {
+        $('.top-nav').css('transform', 'translateY(0)')
+    }
+    lastScrollTop = st;
+});
+
+
+
+
 // Show Search Bar when scrolling
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 450;
 
-    if(y_scroll_pos > scroll_pos_test) {
+    if (y_scroll_pos > scroll_pos_test) {
         $('.top-nav-search > input').css('opacity', '1');
     }
 });
