f3e6665eb8a6397a49e7cb2b9212220675c8e390
mads-jakobsen <mads.jakobsen@gmail.com>
2016-06-29 19:09:44 +0200

    Added socket and sample data
    Former-commit-id: 37f6eed784dd7df936dbde39f8d1ac304d041109

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var json = require(`./json/items/landing_page/popular.json`);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Express'
-  });
-});
+router.get('/', function(req, res){
+    res.render('generic', {
+        content: json,
+        title: "Title",
+        desc: "Desc"
+  })
+})
 
 module.exports = router;
