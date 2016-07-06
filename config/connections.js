dae5f5fef22f82bc52cbc9034d6ba5dff00158d6
lLDGlichigo2 <tomtrommler777@gmail.com>
2016-07-06 23:08:52 +0200

    prepared db for testing - set up connection in controllers and models - included raw db entry-creating script
    Former-commit-id: 392bb8fb4d03adaf376b392c881108c8496a0150

--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+   WARFRAMENEXUS: {
+     adapter: 'sails-mysql',
+     host: 'localhost',
+     user: 'root', //optional
+     password: 'root', //optional
+     database: 'warframenexus' //optional
+   },
 
   /***************************************************************************
   *                                                                          *
