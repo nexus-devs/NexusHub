3d3cb56c1406e61c3f550fd9712443f390292113
mads-jakobsen <mads.jakobsen@gmail.com>
2016-07-13 22:57:54 +0200

    Added comments to backend logic

--- api/controllers/RequestsController.js
+++ api/controllers/RequestsController.js
@@ -5,14 +5,18 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+// Import Foreign Collections
 var projectController = require('./ItemListController');
 
 module.exports = {
+
+    // Process Request
     create: function(req, res) {
-         var request = req.body
-         console.log(request)
+         var request = req.body // catch data sent from python POST
 
          function ProcessRequest(err, request){
+
+            //Convert data from Python to similar variables
             var REQ_User = request.username;
             var REQ_TO = request.to;
             var REQ_Main = request.item;
@@ -29,8 +33,7 @@ module.exports = {
             // Check if component matches item
             function validity(item, component) {
 
-                console.log("item " + item)
-
+                // If No component -> nothing to compare -> Full item
                 if (REQ_Comp === 'null') {
                     var request_status = 'valid'
                 } else {
@@ -39,9 +42,10 @@ module.exports = {
                     ItemList.find({
                         title: item
                     }).exec(function (err, itemschema) {
-                        console.log("item " + item)
-                        console.log(itemschema)
+
+                        // Set validity to false by default, changes when component is found
                         var request_status = 'false'
+                        // Check if component found for each item in ItemList
                         itemschema.components.forEach(function (itemcomponent) {
                             if (itemcomponent === component) {
                                 var request_status = 'valid'
@@ -50,11 +54,13 @@ module.exports = {
                     })
                 }
 
+                // Return if the Component was found for the respective item
                 return (request_status)
             }
 
 
-            console.log(validity(REQ_Main, REQ_Comp))
+            var request_validity = validity(REQ_Main, REQ_Comp)
+            console.log(request_validity)
 
             // Do other stuff here, if validity === 'valid'
 
