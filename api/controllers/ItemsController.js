a936b3f675397791466385da67cee405cb11ae37
mads-jakobsen <mads.jakobsen@gmail.com>
2016-07-15 22:22:18 +0200

    improved header fluff, added dynamic item caching base
    Former-commit-id: f32be6f202e757224ca2dad7526150125142be7d

--- api/controllers/ItemsController.js
+++ api/controllers/ItemsController.js
@@ -18,31 +18,85 @@ module.exports = {
         var itemname = capitalize(url.split('/').pop().toLowerCase())
 
         // Validate entered URL (if done manually)
-        Itemcache.find({
-            Title: itemname
-        }).exec(function (err, itemobj) {
-            console.log(itemobj)
-            var itembase = capitalize(urlbase[1])
-            var itemname = capitalize(url.split('/').pop().toLowerCase())
+        async.waterfall([
 
-            if (typeof itemobj[0] !== 'undefined') {
-                var itembase = itemobj[0].Type
-                var itemname = itemobj[0].Title
-
-
-
-
-                return res.view('item', {
-                    HeaderTitle: `${itemname} ${itembase} - WarframeNexus`,
-                    itemdata: itemobj[0],
-                    css: "../css/",
-                    js: "../js/",
-                    img: "../img/"
+            // Get Item information
+            function getItemInfo(callback) {
+                ItemList.find({
+                    name: itemname
+                }).exec(function (err, item) {
+                    if (typeof item[0] === 'undefined') {
+                        res.notFound(`${itemname} ${itembase} couldn't be found. Please check your spelling`)
+                    } else {
+                        callback(null, item)
+                    }
                 })
-            } else {
-                res.notFound(`${itemname} ${itembase} couldn't be found. Please check your spelling`)
+            },
+
+
+            // Check if item has been updated
+            function checkUpdate(item, callback) {
+                if (item[0].update === 'pending') {
+                    callback(null, item)
+                } else {
+                    Itemcache.find({
+                        Title: itemname
+                    }).exec(function (err, itemobj) {
+                        var itembase = itemobj[0].Type
+                        var itemname = itemobj[0].Title
+
+                        return res.view('item', {
+                            HeaderTitle: `${itemname} ${itembase} - WarframeNexus`,
+                            itemdata: itemobj[0],
+                            css: "../css/",
+                            js: "../js/",
+                            img: "../img/"
+                        })
+                    })
+                }
+                },
+
+
+            // Generate Item Stats from requests
+            function generateItem(item, callback){
+
+                Users.find({
+                    'requests.title': itemname,
+                }).exec(function (err, user){
+
+                    // Finds all users that requested item
+                    user.forEach(function(request){
+
+                        // Select all requests by users
+                        request.requests.forEach(function(req_item){
+
+                            // Check if request is searched item
+                            if (req_item.title === itemname){
+                                var itemcount = 0;
+                                var components = item[0].components // component schema
+
+                                // Loop through each component and check if in component schema
+                                req_item.components.forEach(function(req_component){
+                                    console.log(req_component)
+                                    components.forEach(function(component){
+
+                                        if (component === req_component.name){
+                                            // increase stats (value, count -> generate daily average, add count to main supdem)
+                                            // if set > also add visible: false
+                                            // with values above: append to itemname in itemcache
+                                            // if component has count of 0: values = 'null'
+                                            // > clear values and loop through next component.
+                                            console.log(req_component.name)
+                                            console.log(req_component.data)
+                                        }
+                                    })
+                                })
+                            }
+                        })
+                    })
+                })
             }
-        })
+        ])
     },
 
 
