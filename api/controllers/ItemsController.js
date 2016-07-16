ca185bce71bc014acafb99356b348f6695544b33
mads-jakobsen <mads.jakobsen@gmail.com>
2016-07-16 05:48:22 +0200

    further advanced dynamic item generation logic
    Former-commit-id: f6e258f735a94813f25604f9b182625cf4a979ee

--- api/controllers/ItemsController.js
+++ api/controllers/ItemsController.js
@@ -6,7 +6,7 @@
  */
 //Capitalize function
 function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 
@@ -16,154 +16,209 @@ module.exports = {
         var urlbase = url.split('/')
         var itembase = capitalize(urlbase[1])
         var itemname = capitalize(url.split('/').pop().toLowerCase())
+        var timerange = 7 // This would usually be included in POST method of time range selection in main screen
 
-        // Validate entered URL (if done manually)
+
         async.waterfall([
 
-            // Get Item information
+            // Validate entered URL (if done manually)
             function getItemInfo(callback) {
-                    ItemList.find({
-                        name: itemname
-                    }).exec(function (err, item) {
-                        if (typeof item[0] === 'undefined') {
-                            res.notFound(`${itemname} ${itembase} couldn't be found. Please check your spelling`)
-                        } else {
-                            callback(null, item)
-                        }
-                    })
+                ItemList.find({
+                    name: itemname
+                }).exec(function (err, item) {
+                    if (typeof item[0] === 'undefined') {
+                        res.notFound(`${itemname} ${itembase} couldn't be found. Please check your spelling`)
+                    } else {
+                        callback(null, item)
+                    }
+                })
             },
 
 
             // Check if item has been updated
             function checkUpdate(item, callback) {
-                    if (item[0].update === 'pending') {
-                        callback(null, item)
-                    } else {
-                        Itemcache.find({
-                            Title: itemname
-                        }).exec(function (err, itemobj) {
-                            var itembase = itemobj[0].Type
-                            var itemname = itemobj[0].Title
+                if (item[0].update === 'pending') {
+                    callback(null, item)
+                } else {
+                    Itemcache.find({
+                        Title: itemname
+                    }).exec(function (err, itemobj) {
+                        var itembase = itemobj[0].Type
+                        var itemname = itemobj[0].Title
 
-                            return res.view('item', {
-                                HeaderTitle: `${itemname} ${itembase} - WarframeNexus`,
-                                itemdata: itemobj[0],
-                                css: "../css/",
-                                js: "../js/",
-                                img: "../img/"
-                            })
+                        return res.view('item', {
+                            HeaderTitle: `${itemname} ${itembase} - WarframeNexus`,
+                            itemdata: itemobj[0],
+                            css: "../css/",
+                            js: "../js/",
+                            img: "../img/"
                         })
-                    }
+                    })
+                }
                 },
 
 
             // Generate Item Stats from requests
             function generateItem(item, callback) {
-                    var components = item[0].components // component schema
-                    components.push('Set')
+                var components = item[0].components // component schema
+                components.push('Set')
 
-                    // Loop through each component and check if requests contain component
-                    components.forEach(function (component) {
+                console.log('item: ' + itemname)
+                // Loop through each component and check if requests contain component
+                components.forEach(function (component) {
 
-                        async.waterfall([
+                    async.waterfall([
 
                         // Find all users offering item
                         function findUsers(callback) {
-                                Users.find({
-                                    'requests.title': itemname,
-                                }).exec(function (err, user) {
-                                    callback(null, user)
-                                })
+                            Users.find({
+                                'requests.title': itemname,
+                            }).exec(function (err, user) {
+                                callback(null, user)
+                            })
 
-                                //
-                                }, function log(user, callback){
-                                    console.log('COMPARING WITH: ' + component)
+                        // Generate values for each Component
+                                },
+                        function log(user, callback) {
+                            console.log('component: ' + component)
 
-                                    // For each user, check if item in each request
-                                    user.forEach(function (request) {
-                                        request.requests.forEach(function (req_item) {
-                                            // Validate request belonging to item
-                                            if (req_item.title === itemname) {
-                                                req_item.components.forEach(function (req_component) {
-                                                    if (component === req_component.name) {
-                                                        console.log(req_component.name)
-                                                        console.log(req_component.data)
+                            // Clear values when starting with new component // generate array w/ 0 for timerange
+                            var comp_data = []
+                            var comp_count = []
+                            for (var i = 0; i < timerange; i++) {
+                                comp_count.push(0)
+                            }
+                            var comp_val = []
+                            for (var i = 0; i < timerange; i++) {
+                                comp_val.push(0)
+                            }
+                            var WTB = 0
+                            var WTS = 0
+
+
+                            // For each user, check if item in each request (loop through every relevant request)
+                            user.forEach(function (user) {
+                                user.requests.forEach(function (req_item) {
+                                    if (req_item.to === 'WTB'){
+                                        WTB++
+                                    } else{
+                                        WTS++
+                                    }
+
+
+                                    // Validate request belonging to item
+                                    if (req_item.title === itemname) {
+                                        req_item.components.forEach(function (req_component) {
+
+                                            // Check Time between Request and now
+                                            var prevTime = new Date(req_item.updatedAt);
+                                            var thisTime = new Date();
+                                            var diff = thisTime.getTime() - prevTime.getTime();
+                                            var delta = (diff / (1000 * 60 * 60 * 24));
+
+                                            // Check if Request has been comitted within timerange
+                                            if (component === req_component.name && delta < timerange) {
+
+                                                // Generate data array
+                                                for (var i = 0; i < timerange; i++) {
+                                                        // If request at 'i' day, value and position to according place
+                                                    if (Math.floor(delta) === i) {
+                                                        comp_val[i] = comp_val[i] + req_component.data
+                                                        comp_count[i]++
                                                     }
-                                                })
+                                                }
                                             }
                                         })
-                                    })
+                                    }
+                                })
+                            })
+
+                            // Take average of value divided by count
+                            for (var i = 0; i < timerange; i++) {
+                                if (comp_val[i] !== 0) {
+                                   comp_data.push((comp_val[i] / comp_count[i]))
+                                } else {
+                                comp_data.push('null')
+                                }
+                            }
+
+                            // Reverse array (newest at end to match chart)
+                            comp_data.reverse();
+                            console.log(comp_data)
+
+                            // Generate average value
+                            var avg = 0
+                            for (var i = 0; i < comp_data.length; i++){
+                                if (comp_data[i] === 'null'){
+                                    var current_value = 0
+                                } else {
+                                    var current_value = comp_data[i]
+                                }
+                                avg = avg + current_value
+                            }
+                            // Realtime avg
+                            console.log('comp_val_rt: ' + avg/comp_data.length)
+
+                            // Normal avg
+                            avg = Math.floor((avg/comp_data.length)).toString() + 'p'
+                            console.log('avg: ' + avg)
+                            console.log('----------------------')
+
+
+
+                            // When all data is collected: create database entry
+                            // Dont forget to set update to false in itemlist
                                 }
 
                     ])
 
-                        // Finds all users that requested item
-                        //user.forEach(function (request) {
-
-                        // Select all requests by users
-                        //request.requests.forEach(function (req_item) {
-
-                        // Check if request is searched item
-                        //if (req_item.title === itemname) {
-
-                        //req_item.components.forEach(function (req_component) {
-                        //console.log('comparison: ' + req_component)
-                        //if (component === req_component.name) {
-
-                        // increase stats (value, count -> generate daily average, add count to main supdem)
-                        // if set > also add visible: false
-                        // with values above: append to itemname in itemcache
-                        // if component has count of 0: values = 'null'
-                        // > clear values and loop through next component.
-                        //console.log(req_component.name)
-                        //console.log(req_component.data)
-
-                    })
+                })
+                //console.log('Supply: ' + WTB)
                 }
         ])
-},
+    },
 
 
-// Search function
-search: function (req, res) {
-    var fullstring = req.query.item
-    var stringArray = fullstring.split(" ")
-    var viewrendered = 'false'
-    var i = 0
+    // Search function
+    search: function (req, res) {
+        var fullstring = req.query.item
+        var stringArray = fullstring.split(" ")
+        var viewrendered = 'false'
+        var i = 0
 
-    // Check for each search term
-    async.forEach(stringArray, function (string, callback) {
+        // Check for each search term
+        async.forEach(stringArray, function (string, callback) {
 
-            async.waterfall([
+                async.waterfall([
 
                 // Try retrieving item name
                 function retrieveItem(callback) {
-                        ItemList.find({
-                            _id: capitalize(string)
-                        }).exec(function (err, itemobj) {
-                            if (err) {
-                                callback(err, null)
-                                return
-                            }
-                            callback(null, itemobj)
-                        })
+                            ItemList.find({
+                                _id: capitalize(string)
+                            }).exec(function (err, itemobj) {
+                                if (err) {
+                                    callback(err, null)
+                                    return
+                                }
+                                callback(null, itemobj)
+                            })
 
-                        // Check if item was found
+                            // Check if item was found
                 },
                         function checkValidity(itemobj, callback) {
-                        if (typeof itemobj[0] !== 'undefined') {
-                            var itembase = itemobj[0].type
-                            var itemname = itemobj[0].name
-                            return res.redirect(`../../${itembase}/${itemname}`)
-                        } else {
-                            res.notFound(`${fullstring} couldn't be found. Please check your spelling`)
-                        }
+                            if (typeof itemobj[0] !== 'undefined') {
+                                var itembase = itemobj[0].type
+                                var itemname = itemobj[0].name
+                                return res.redirect(`../../${itembase}/${itemname}`)
+                            } else {
+                                res.notFound(`${fullstring} couldn't be found. Please check your spelling`)
+                            }
 
                 }
 
             ]) // End async.waterfall
-            callback();
-        }) // End Query
+                callback();
+            }) // End Query
 
-}
+    }
 }
