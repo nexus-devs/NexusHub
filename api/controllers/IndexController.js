5dcdcc02e2af42f55d5641a22eec96b02c09fdc9
mads-jakobsen <mads.strandbygaard.jakobsen@gmail.com>
2016-07-25 13:56:16 +0200

    Synced with master
    Former-commit-id: 7dc8a7681fba417df283fbadadea706b7b22ab72

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -12,9 +12,23 @@ module.exports = {
             var pop_2 = ''
             var pop_3 = ''
 
+            // Random Number for last 2 entries (need some SEO)
+            var ran_one = (Math.floor(Math.random() * (items.length)))
+            var ran_two = (Math.floor(Math.random() * (items.length)))
+            var ran_obj_1 = {}
+            var ran_obj_2 = {}
+            var i = 0
+
             items.forEach(function (item) {
                 var popularity = (item.SupDemNum[0] + item.SupDemNum[1])
-                if (popularity > pop_old_1) {
+
+                if (i === ran_one){
+                    ran_obj_1 = item
+                }
+                else if (i === ran_two){
+                    ran_obj_2 = item
+                }
+                else if (popularity > pop_old_1) {
                     pop_obj_3 = pop_obj_2
                     pop_obj_2 = pop_obj_1
                     pop_old_1 = popularity
@@ -31,10 +45,10 @@ module.exports = {
                         }
                     }
                 }
-
+                i++
             })
 
-            var pop_items = [pop_obj_1, pop_obj_2, pop_obj_3] //Add pop 3, pop 2
+            var pop_items = [pop_obj_1, pop_obj_2, pop_obj_3, ran_obj_1, ran_obj_2]
 
             return res.view('index', {
                 content: pop_items,
