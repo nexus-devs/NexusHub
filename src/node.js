548b2d35fffef37a61e07c7f606a38995c16c6dd
Kaptard <mads.strandbygaard.jakobsen@gmail.com>
2017-03-22 22:55:15 +0100

    made exception handling compatible w/ new client-side auto-ratelimiting
    Former-commit-id: a38aa3bef4b506ad1ba8c01a05eff3925154eea2

--- src/node.js
+++ src/node.js
@@ -13,7 +13,8 @@ cli.time(process.env.src_id, cli.chalk.reset("Port: " + process.env.src_port) + 
 const Nexus = require('../../NexusStatsApi/index.js')
 const api = new Nexus({
     game: 'warframe',
-    use_socket: false,
+    use_socket: true,
+    //ignore_limiter: true, // Root-access has no limiter
 
     // Example Credentials taken from <nexus-auth>/register
     user_key: 'alaIz8cqU7cv2d336ssww28f0DufMbdJaMXZyg6BlaSiK453pspSygd81X5kLYMk',
@@ -33,7 +34,7 @@ api.on('ready', () => {
         api.getItem({
             name: 'Nikana Prime'
         }).then(item => {
-            cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+            //cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
         })
     }, 15000)
 
@@ -42,7 +43,7 @@ api.on('ready', () => {
         api.getItem({
             name: 'Nikana Prime'
         }).then(item => {
-            cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+            //cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
         })
     }, 25000)
 
@@ -54,14 +55,12 @@ api.on('ready', () => {
     /**
      * get nikana prime stats test command
      */
-    //for(var i = 0; i < 200; i++){
-    //setTimeout(() => {
+    for(var i = 0; i < 20; i++){
 
     api.getItem({
         name: 'Nikana Prime'
     }).then(item => {
-        cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+        //cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
     })
-    //}, 2)
-    //}
+    }
 })
