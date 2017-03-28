4b364100bf46a39112024e7e8a00037df163da00
Kaptard <mads.strandbygaard.jakobsen@gmail.com>
2017-03-28 03:25:43 +0200

    Rebuilt dbs node into proper class structure
    Former-commit-id: 84f71a629b94641de8e8c4bf91fb9dc9f0e65cfa

--- dbs/node.js
+++ dbs/node.js
@@ -1,59 +1,87 @@
 #!/usr/bin/env node
 
 /**
- * Start Logging Timers
+ * Globals
  */
 global.cli = require('../config/log/logger.js')
+
+
+/**
+ * Time measurement
+ */
 cli.time(process.env.src_id, cli.chalk.reset("Port: " + process.env.src_port) + cli.chalk.green(' [online]'))
 
 
 /**
- * Load Nexus API
+ * Module Dependencies
  */
 const Nexus = require('../../nexus-stats-api/index.js')
-const api = new Nexus({
-    game: 'warframe',
-    use_socket: true,
-    //ignore_limiter: true, // Root-Access not getting limited anyway
-
-    // Example Credentials taken from <nexus-auth>/register
-    user_key: 'alaIz8cqU7cv2d336ssww28f0DufMbdJaMXZyg6BlaSiK453pspSygd81X5kLYMk',
-    user_secret: 'DNQt6oBMzTisRPHDXqbpxqEYpvtlpn2aHLMuKdEJMnrFNi6qxP87Al9iS3eP9Yui'
-})
-
-cli.timeEnd(process.env.src_id, cli.chalk.reset("Port: " + process.env.src_port) + cli.chalk.green(' [online]'))
 
 
 /**
- * When connected to API
+ * Describes parent class which controls all objects handling input/output
  */
-api.on('ready', () => {
-
-    // Token Expiration Test
-    setTimeout(() => {
-        api.getItem('Nikana Prime').then(item => {
-            cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
-        })
-    }, 15000)
-
-        // Token Expiration Test
-    setTimeout(() => {
-        api.getItem('Nikana Prime').then(item => {
-            cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
-        })
-    }, 25000)
-
-
-    //api.connection.client.socket.emit("GET", "yourmom", ack => {
-    //    console.log('\n' + ack.body)
-    //})
+class dbs {
 
     /**
-     * get nikana prime stats test command
+     * Set Up Nexus Client
      */
-    for(var i = 0; i < 10; i++){
-    api.getItem('Nikana Prime', {component: "Blade"}).then(item => {
-        //cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
-    })
+    constructor() {
+
+        this.client = new Nexus({
+            game: 'warframe',
+            use_socket: true,
+            //ignore_limiter: true, // Root-Access not getting limited anyway
+            user_key: 'alaIz8cqU7cv2d336ssww28f0DufMbdJaMXZyg6BlaSiK453pspSygd81X5kLYMk',
+            user_secret: 'DNQt6oBMzTisRPHDXqbpxqEYpvtlpn2aHLMuKdEJMnrFNi6qxP87Al9iS3eP9Yui'
+        })
+
+        this.client.on('ready', () => {
+
+            // Finish time measurement
+            cli.timeEnd(process.env.src_id, cli.chalk.reset("Port: " + process.env.src_port) + cli.chalk.green(' [online]'))
+
+            // Launch test requests
+            this.test()
+        })
     }
-})
+
+
+    /**
+     * Test Commands
+     */
+    test() {
+
+        // Token Expiration Test
+        setTimeout(() => {
+            this.client.getItem('Nikana Prime').then(item => {
+                cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+            })
+        }, 15000)
+
+        // Token Expiration Test
+        setTimeout(() => {
+            this.client.getItem('Nikana Prime').then(item => {
+                cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+            })
+        }, 25000)
+
+        // Circumvent standard module connection & send malformed request
+        this.client.connection.client.socket.emit("GET", "yourmom", ack => {
+            console.log('\n' + ack.body)
+        })
+
+        // Rate Limit Testing
+        for (var i = 0; i < 10; i++) {
+            this.client.getItem('Nikana Prime', {
+                component: "Blade",
+                timestart: 3824983243892,
+                timeend: 3894982348932
+            }).then(item => {
+                //cli.log(process.env.src_id, 'neutral', 'API      | ' + item, 'in')
+            })
+        }
+    }
+}
+
+module.exports = new dbs()
