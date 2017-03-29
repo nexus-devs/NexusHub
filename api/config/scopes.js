ea81df869838e65801d8aecde53f1bdd93f4c5b1
Kaptard <mads.strandbygaard.jakobsen@gmail.com>
2017-03-29 22:45:35 +0200

    Added Core Structure
    Former-commit-id: d2482e2fedbe17779546c1ed4a51638801cb1b72

---
+++ api/config/scopes.js
@@ -0,0 +1,10 @@
+/**
+ * Placeholder file for hierarchy of access scopes
+ * Scopes are used to authorize access to certain endpoints
+ */
+
+module.exports = () => {
+    let standard = ['standard-read', 'standard-read-write']
+    let elevated = ['elevated-read', 'elevated-read-write']
+    let root = ['root-read', 'root-read-write']
+}
---
+++ core/methods/Method.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Class describing generic database/calculation methods
+ * Any lower-level method extends this class
+ */
+class Method{
+    constructor(){
+        //load db connection
+    }
+}
