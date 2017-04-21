044aacc5ed2c383b81c19d37818696e6729b0a4a
Nakroma <tarackobarack@gmail.com>
2017-04-21 18:39:28 +0200

    Changed market method fields again
    Former-commit-id: f1a9ca74a1ea379be1009ecaa9a6fd858d518b3a

--- core/methods/warframe/v1/items/market.js
+++ core/methods/warframe/v1/items/market.js
@@ -19,6 +19,22 @@ class Market extends Method {
                 required: true,
                 description: "Specifies item component to look up. No component returns full set data."
             },
+            {
+                name: "timestart",
+                type: "number",
+                default: () => {
+                    return new Date().getTime() // current time
+                },
+                description: "Returns data recorded between timestart and timeend."
+            },
+            {
+                name: "timeend",
+                type: "number",
+                default: () => {
+                    return new Date(new Date().setDate(new Date().getDate() - 21)) / 1 // 3 weeks ago
+                },
+                description: "Returns data recorded between timestart and timeend."
+            }
         ]
     }
 
