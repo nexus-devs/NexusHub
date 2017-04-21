259240d1d9823e92af9bcaddb4783f7df98c0d89
Kaptard <kilian.volb@protonmail.com>
2017-04-21 20:18:06 +0200

    put mongo client into method blueprint directly
    Former-commit-id: 672d70c4501c57c70e166459b587d41c24ce8fd4

--- core/Method.js
+++ core/Method.js
@@ -1,11 +1,21 @@
 'use strict'
 
 /**
+ * Connect to mongodb for all methods
+ */
+let mongodb = require("mongodb").MongoClient
+let db = null
+mongodb.connect(process.env.mongo_url, (err, connected) => {
+    if(err) throw(err)
+    db = connected
+})
+
+/**
  * Class describing generic database/calculation methods
  * Any lower-level method extends this class
  */
 class Method{
-    constructor(db){
+    constructor(){
 
         // Default Config for API node
         this.schema = {
