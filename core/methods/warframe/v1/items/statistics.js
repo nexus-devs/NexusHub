a1adacf180b383e1df262cf1f534a457839ebaa2
Nakroma <tarackobarack@gmail.com>
2017-04-22 15:36:56 +0200

    Overhauled statistics method
    Former-commit-id: e48c923b9561fb035a00c46b3aa4eaaab6376465

--- core/methods/warframe/v1/items/statistics.js
+++ core/methods/warframe/v1/items/statistics.js
@@ -38,8 +38,8 @@ class Statistics extends Method {
             {
                 name: "interval",
                 type: "number",
-                default: 1,
-                description: "Days between each interval."
+                default: 7,
+                description: "Intervals to split the time in."
             }
         ]
     }
@@ -61,10 +61,111 @@ class Statistics extends Method {
             if (component != "") query['component'] = parseInt(component)
 
             // Query and resolve results
-            this.db.collection('dummy_requests').find(query).toArray(function(err, result) {
+            this.db.collection('dummy_requests').find(query).toArray((err, result) => {
                 if (err) reject(err)
 
-                resolve(result)
+                // Document to return
+                let doc = {
+                    title: "Saryn Prime", // TODO: Change this to reflect the actual item
+                    type: "Prime",
+                    supply: { count: 0, percent: 0 },
+                    demand: { count: 0, percent: 0 },
+                    components: []
+                }
+
+                // Query vars
+                let resultLength = result.length
+                let currentInterval = 0 // Interval helper
+                let componentIndex = 0 // Cached variable for speed
+                let currentRequest // Cached variable for speed
+                let intervalSize = (timestart - timeend) / interval // How big is the interval gap
+                let p = 0   // Cached variable for price
+
+                for (let i = resultLength - 1; i >= 0; i--) {
+                    currentRequest = result[i]
+                    componentIndex = doc.components.findIndex(x => x.name == currentRequest.component)
+
+                    // Create component if it doesn't exist
+                    if (componentIndex == -1) {
+                        // Component blueprint
+                        let component = {
+                            name: currentRequest.component,
+                            avg: 0,
+                            min: Number.POSITIVE_INFINITY,
+                            max: Number.NEGATIVE_INFINITY,
+                            supply: { count: 0, percent: 0 },
+                            demand: { count: 0, percent: 0 },
+                            interval: []
+                        }
+
+                        // Fill interval array
+                        for (let j = 0; j < interval; j++) {
+                            let intervalObj = {     // Helper obj for field creation
+                                avg: 0,
+                                min: Number.POSITIVE_INFINITY,
+                                max: Number.NEGATIVE_INFINITY,
+                                supply: { count: 0, percent: 0 },
+                                demand: { count: 0, percent: 0 }
+                            }
+
+                            component.interval.push(intervalObj)
+                        }
+
+                        // Set index and push object
+                        componentIndex = doc.components.push(component) - 1
+                    }
+
+                    // Find current interval
+                    currentInterval = Math.floor((currentRequest.createdAt.getTime() - timeend) / intervalSize)
+
+                    // Add to current interval
+                    p = currentRequest.price
+                    doc.components[componentIndex].interval[currentInterval].avg += p
+                    if (p < doc.components[componentIndex].interval[currentInterval].min) doc.components[componentIndex].interval[currentInterval].min = p
+                    if (p > doc.components[componentIndex].interval[currentInterval].max) doc.components[componentIndex].interval[currentInterval].max = p
+                    if (currentRequest.offer == "selling") {
+                        doc.components[componentIndex].interval[currentInterval].supply.count++
+                    } else {
+                        doc.components[componentIndex].interval[currentInterval].demand.count++
+                    }
+                }
+
+                // Loop through intervals
+                let offerCount = 0
+                for (let i = 0; i < doc.components.length; i++) {
+                    for (let j = 0; j < doc.components[i].interval.length; j++) {
+                        // Calculate avg and supply/demand percentages
+                        offerCount = doc.components[i].interval[j].supply.count + doc.components[i].interval[j].demand.count
+                        doc.components[i].interval[j].avg = doc.components[i].interval[j].avg / offerCount
+                        doc.components[i].interval[j].supply.percent = doc.components[i].interval[j].supply.count / offerCount
+                        doc.components[i].interval[j].demand.percent = doc.components[i].interval[j].demand.count / offerCount
+
+                        // Add interval vars on component vars
+                        doc.components[i].avg += doc.components[i].interval[j].avg
+                        doc.components[i].supply.count += doc.components[i].interval[j].supply.count
+                        doc.components[i].demand.count += doc.components[i].interval[j].demand.count
+                        if (doc.components[i].interval[j].min < doc.components[i].min) doc.components[i].min = doc.components[i].interval[j].min
+                        if (doc.components[i].interval[j].max > doc.components[i].max) doc.components[i].max = doc.components[i].interval[j].max
+                    }
+
+                    // Calculate avg and supply/demand percentages
+                    offerCount = doc.components[i].supply.count + doc.components[i].demand.count
+                    doc.components[i].avg = doc.components[i].avg / doc.components[i].interval.length
+                    doc.components[i].supply.percent = doc.components[i].supply.count / offerCount
+                    doc.components[i].demand.percent = doc.components[i].demand.count / offerCount
+
+                    // Add component vars to document vars
+                    doc.supply.count += doc.components[i].supply.count
+                    doc.demand.count += doc.components[i].demand.count
+                }
+
+                // Calculate document supply/demand percentages
+                offerCount = doc.supply.count + doc.demand.count
+                doc.supply.percent = doc.supply.count / offerCount
+                doc.demand.percent = doc.demand.count / offerCount
+
+                // Return document
+                resolve(doc)
             })
         })
     }
