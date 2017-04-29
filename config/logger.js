3dbf91df507e0ea046be032f5b9874d8becc4dea
Kaptard <kilian.volb@protonmail.com>
2017-04-29 02:02:37 +0200

    fixed mongourl
    Former-commit-id: d49f81522d9a7f454e66ffc4a09339e5da84bf12

--- config/logger.js
+++ config/logger.js
@@ -19,12 +19,12 @@ class Logger {
      */
     intro() {
         console.log(" ")
-        console.log(chalk.grey(":: Nexus-Stats Warframe Server v2.1.1"))
+        console.log(chalk.grey(":: Nexus-Stats Warframe Server v.2.1.1"))
         console.log(chalk.grey(":: Launching blitz.js stack..."))
         console.log(" ")
         console.log(chalk.grey("---------------------------------------------------------------"))
         console.log(" ")
-        console.log("                            L;                                \n                             t@8;                             \n                              ;@@@0;                          \n                               ,@@@@@0;                       \n                                 0@@@@@@0:                    \n                  8tttt1i;:,      L@@@@@@8                    \n                  @@fttttt1.       1@@@@@8                    \n                  @@@Lttt,          ;@@@@8                    \n                  @@@@G;           itf@@@8                    \n                  @@@@@G         ;ttttt8@8                    \n                  @@@@@@8.     .itttttttG8                    \n                  @@@@@@@@:           .:;t                    \n                   .L@@@@@@1                                  \n                      .L@@@@L                                 \n                         .L@@0      " + chalk.green("v2.1.1") + "                   \n                            .L8,                              \n                               ..                  ")
+        console.log("                            L;                                \n                             t@8;                             \n                              ;@@@0;                          \n                               ,@@@@@0;                       \n                                 0@@@@@@0:                    \n                  8tttt1i;:,      L@@@@@@8                    \n                  @@fttttt1.       1@@@@@8                    \n                  @@@Lttt,          ;@@@@8                    \n                  @@@@G;           itf@@@8                    \n                  @@@@@G         ;ttttt8@8                    \n                  @@@@@@8.     .itttttttG8                    \n                  @@@@@@@@:           .:;t                    \n                   .L@@@@@@1                                  \n                      .L@@@@L                                 \n                         .L@@0     " + chalk.green("v.2.1.1") + "                   \n                            .L8,                              \n                               ..                  ")
         console.log(" ")
         console.log(" ")
         console.log(chalk.grey(":: " + new Date()))
