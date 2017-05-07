665740385267716743cb0a1a4e069c2eed2927ab
Nakroma <tarackobarack@gmail.com>
2017-05-07 19:32:21 +0200

    Added simple test server

---
+++ testserver.js
@@ -0,0 +1,43 @@
+"use strict"
+
+/**
+ * blitz.js setup
+ */
+const Blitz = require("blitz-js")({logLevel: "silly", environment: "test"})
+
+
+/**
+ * Big useless intro
+ */
+const intro = require("./config/logger.js")
+
+
+/**
+ * blitz.js authentication server
+ */
+const Auth = require("blitz-js-auth")
+const authOptions = {
+    mongoURL: "mongodb://localhost/warframe-nexus-test"
+}
+blitz.use(new Auth(authOptions))
+
+
+/**
+ * blitz.js api node
+ */
+const API = require("blitz-js-api")
+const apiOptions = {
+    mongoURL: "mongodb://localhost/warframe-nexus-test"
+}
+blitz.use(new API(apiOptions))
+
+
+/**
+ * blitz.js core node
+ */
+const Core = require("blitz-js-core")
+let coreOptions = {
+    endpointPath: __dirname + "/endpoints",
+    mongoURL: "mongodb://localhost/warframe-nexus-test"
+}
+blitz.use(new Core(coreOptions))
