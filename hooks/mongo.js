c40cc26b8ff52089340149ae1356a1ba9046562f
Kaptard <kilian.volb@protonmail.com>
2017-05-13 18:30:26 +0200

    Fixed typos. Improved logging.

--- hooks/mongo.js
+++ hooks/mongo.js
@@ -1,28 +1,30 @@
 "use strict"
 
 /**
- * MongoDB related hooks
+ * Dependencies
  */
-
-
-
+const mongodb = require('mongodb').MongoClient
 
 module.exports = {
+
     /**
      * Ensures mongodb indexes and creates missing ones
      */
-     mongoVerifyIndexes: () => {
-        let mongodb = require('mongodb').MongoClient
-
+     mongoVerifyIndices: () => {
         mongodb.connect(blitz.config.core.mongoURL + "?socketTimeoutMS=0", function(err, db) {
             if (err) throw err
 
             // TODO: Change item index to text
-            blitz.log.info("creating and ensuring mongo indexes")
+            console.log(" ")
+            blitz.log.info("mongoHook | verifying request indices")
             db.collection('requests').createIndex({'component': 1})
+            blitz.log.verbose("mongoHook | component index verified")
             db.collection('requests').createIndex({'item': 1})
+            blitz.log.verbose("mongoHook | item index verified")
             db.collection('requests').createIndex({'timestamp': -1})
+            blitz.log.verbose("mongoHook | timestamp index verified")
             db.collection('requests').createIndex({'timestamp': -1, 'item': 1})
+            blitz.log.verbose("mongoHook | compound timestamp/item index verified\n")
         })
     }
 }
