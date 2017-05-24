0dc4ba8ad3c172b8221f290984e13c2d64714f05
Kaptard <kilian.volb@protonmail.com>
2017-05-24 20:35:55 +0200

    Adapt to new module updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * blitz.js setup
  */
-const Blitz = require("blitz-js")({logLevel: "info"})
+const Blitz = require("blitz-js")({logLevel: "verbose"})
 
 
 /**
@@ -15,16 +15,15 @@ const intro = require("./config/logger.js")
 /**
  * Import hooks
  */
-const mongoHooks = require('./hooks/mongo')
+let mongoHooks = require('./hooks/mongo')
 
 
 /**
  * blitz.js authentication server
  */
-const Auth = require("../../blitz/blitz.js-auth/index.js")
+const Auth = require("blitz-js-auth")
 const authOptions = {
-    mongoURL: "mongodb://localhost/warframe-nexus",
-    exp: "10s"
+    mongoURL: "mongodb://localhost/warframe-nexus"
 }
 blitz.use(new Auth(authOptions))
 
@@ -32,7 +31,7 @@ blitz.use(new Auth(authOptions))
 /**
  * blitz.js api node
  */
-const API = require("../../blitz/blitz.js-api/index.js")
+const API = require("blitz-js-api")
 const apiOptions = {
     mongoURL: "mongodb://localhost/warframe-nexus"
 }
@@ -42,7 +41,7 @@ blitz.use(new API(apiOptions))
 /**
  * blitz.js core node
  */
-const Core = require("../../blitz/blitz.js-core/index.js")
+const Core = require("blitz-js-core")
 let coreOptions = {
     endpointPath: __dirname + "/endpoints",
     mongoURL: "mongodb://localhost/warframe-nexus"
@@ -53,9 +52,8 @@ blitz.use(new Core(coreOptions))
 
 /**
  * blitz.js view node
- */
 const View = require("../../blitz/blitz.js-view/index.js")
 let viewOptions = {
-
 }
 blitz.use(new View(viewOptions))
+ */
