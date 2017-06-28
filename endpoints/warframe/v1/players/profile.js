4d180c96e85761470a1cb2b8f8ce6e66ef447778
Kaptard <kilian.volb@protonmail.com>
2017-06-28 22:38:25 +0200

    implement lazy caching
    Former-commit-id: 778cebddab6b1511460c2cca95f8f0a0c924792f

--- endpoints/warframe/v1/players/profile.js
+++ endpoints/warframe/v1/players/profile.js
@@ -19,31 +19,33 @@ class Request extends Endpoint {
      */
     main(username) {
         return new Promise((resolve, reject) => {
-            if(username.length > 16) {
+            if (username.length > 16) {
                 reject("Username can't have more than 16 characters.")
             } else {
                 this.db.collection("players").findOne({
                     name: new RegExp("^" + username + "$", "i")
                 }).then((result) => {
 
-                    // User saved? Return cached.
+                    // User saved? Return cached data if younger than 24h
                     if (result) {
-                        resolve(result)
+                        if (new Date - result.updatedAt < 86400000) {
+                            return resolve(result)
+                        } else {
+                            resolve(result)
+                        }
                     }
 
-                    // User not saved, get data
-                    else {
-                        let playerURL = "/warframe/v1/players/" + username + "/profile"
-                        let botURL = "/warframe/v1/bots/getProfile"
+                    // Query user if older than 24h or doesn't exist yet
+                    let playerURL = "/warframe/v1/players/" + username + "/profile"
+                    let botURL = "/warframe/v1/bots/getProfile"
 
-                        this.api.subscribe(playerURL)
-                        this.publish(botURL, username)
+                    this.api.subscribe(playerURL)
+                    this.publish(botURL, username)
 
-                        this.api.on(playerURL, player => {
-                            this.api.connection.client.off(playerURL)
-                            player.mastery ? resolve(player) : reject(username + " could not be found.")
-                        })
-                    }
+                    this.api.on(playerURL, player => {
+                        this.api.connection.client.off(playerURL)
+                        player.mastery ? resolve(player) : reject(username + " could not be found.")
+                    })
                 })
             }
         })
