591e1065a045258a89e3b63d54ccde11acd65dc0
Kilian <kilian.volb@protonmail.com>
2017-08-21 12:39:35 +0200

    move routing logic entirely into view node

--- view/src/app-client.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from './app.js'
-
-const { app, router } = createApp()
-
-// Wait until router has resolved possible async hooks
-router.onReady(() => {
-    app.$mount('app')
-})
