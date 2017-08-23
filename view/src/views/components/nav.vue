e56d2fce115e5da6a302ea0cee0e26fdb3650b2f
Kilian <kilian.volb@protonmail.com>
2017-08-23 16:53:00 +0200

    apply standard js
    Former-commit-id: b5f6a3afe07e154f3f86d2d9f5991130d3ce278c

--- view/src/views/components/nav.vue
+++ view/src/views/components/nav.vue
@@ -1,51 +1,52 @@
 <template>
-    <nav class="row">
-        <div class="col nav-l">
-            <img src="/img/nav/side-nav.svg" alt="Sidebar Navigation" class="ico-24">
-            <router-link to="/" exact>Nexus-Stats</router-link>
-        </div>
-        <div class="col nav-r">
-            <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
-            <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
-            <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
-        </div>
-    </nav>
+  <nav class="row">
+    <div class="col nav-l">
+      <img src="/img/nav/side-nav.svg" alt="Sidebar Navigation" class="ico-24">
+      <router-link to="/" exact>Nexus-Stats</router-link>
+    </div>
+    <div class="col nav-r">
+      <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
+      <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
+      <a href="#"><img src="/img/placeholder.svg" alt="" class="ico-20"><span>placeholder</span></a>
+    </div>
+  </nav>
 </template>
 
 
 <style lang="scss" scoped>
-    @import "../../styles/partials/importer";
-    nav {
-        position: fixed;
-        width: calc(100% - 50px);
-        z-index: 2;
-        padding: 16px 25px;
-        background: rgba(0,0,0,0.8);
-        align-items: center;
-        @include shadow-1;
+  @import "../../styles/partials/importer";
+  nav {
+      position: fixed;
+      width: calc(100% - 50px);
+      z-index: 2;
+      padding: 16px 25px;
+      background: rgba(0,0,0,0.8);
+      align-items: center;
+      @include shadow-1;
 
-        a {
-            margin: 0 15px;
-            &, & span {
-                display: inline-block;
-                vertical-align: middle;
-                margin-left: 3px;
-            }
-        }
-    }
-    .nav-l {
-        a:first-of-type {
-            margin-left:20px;
-        }
-    }
-    .nav-r {
-        text-align: right;
-    }
-    @media (max-width: $breakpoint-s) {
-        .nav-r {
-            span {
-                display: none;
-            }
-        }
-    }
+      a {
+          margin: 0 15px;
+          &,
+          & span {
+              display: inline-block;
+              vertical-align: middle;
+              margin-left: 3px;
+          }
+      }
+  }
+  .nav-l {
+      a:first-of-type {
+          margin-left: 20px;
+      }
+  }
+  .nav-r {
+      text-align: right;
+  }
+  @media (max-width: $breakpoint-s) {
+      .nav-r {
+          span {
+              display: none;
+          }
+      }
+  }
 </style>
--- view/src/views/warframe/index.vue
+++ view/src/views/warframe/index.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="row">
-        <h1>Hi!</h1>
-    </div>
+  <div class="row">
+    <h1>Hi!</h1>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    h1 {
-        font-weight:200;
-        font-size:4em;
-    }
-    .row {
-        height: 100vh;
-        align-items: center;
-        justify-content: center;
-    }
+  h1 {
+      font-weight: 200;
+      font-size: 4em;
+  }
+  .row {
+      height: 100vh;
+      align-items: center;
+      justify-content: center;
+  }
 </style>
