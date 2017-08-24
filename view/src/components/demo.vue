32e19db5ffcaae31734a2aea438a044b01a294b6
Kilian <kilian.volb@protonmail.com>
2017-08-24 21:15:32 +0200

    put blitz api into vue property, add last-request demo
    Former-commit-id: ee34c04134f2bc8d164aa1581dcc3bc01b6614af

--- view/src/components/demo.vue
+++ view/src/components/demo.vue
@@ -13,16 +13,23 @@
     },
 
     asyncData({ store }) {
-      return store.dispatch('setValue', '什么他妈的你他妈的刚才说我？')
+      return store.commit('setValue', '什么他妈的你他妈的刚才说我')
     },
 
     beforeMount() {
-      this.setValue('kketeteskekkeke')
+      this.setLatestRequests()
     },
 
     methods: {
-      setValue(value) {
-        this.$store.dispatch('setValue', value)
+      async setLatestRequests() {
+        this.$blitz.subscribe('/warframe/v1/requests')
+        let requests = await this.$blitz.get('/warframe/v1/requests')
+        this.$store.commit('setValue', requests[0].user)
+        this.$blitz.on('/warframe/v1/requests', request => {
+          requests.pop()
+          requests.unshift(request)
+          this.$store.commit('setValue', requests[0].user)
+        })
       }
     }
   }
