796edc0d77cb7798527262a8f0b626b570afd0e2
Kilian <kilian.volb@protonmail.com>
2017-09-02 19:01:50 +0200

    fix extract-css in production
    Former-commit-id: 90574465b5157c054784a7e7485a89daf18f8b7f

--- config/webpack/vue.config.js
+++ config/webpack/vue.config.js
@@ -1,20 +1,17 @@
 const isProd = blitz.config.local.environment !== "development"
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const extractSass = new ExtractTextPlugin({
-  filename: "[name].[chunkhash].css",
-  disable: !isProd
-})
 
-module.exports = {
-  extractCSS: isProd ? true : false,
-  loaders: isProd ? {
-    scss: extractSass.extract({
-      use: "!css-loader!sass-loader?"
-    })
-  } : {},
-  postcss: [
-    require('autoprefixer')({
-      browsers: ['last 3 versions']
-    })
-  ]
+module.exports = (extractSass) => {
+  return {
+    extractCSS: isProd,
+    loaders: isProd ? {
+      scss: extractSass.extract({
+        use: "!css-loader!sass-loader?"
+      })
+    } : {},
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 3 versions']
+      })
+    ]
+  }
 }
