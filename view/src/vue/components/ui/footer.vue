928327b556e99a3a9809fe6233d84a9ca91a2f33
Kilian <kilian.volb@protonmail.com>
2017-09-02 22:04:34 +0200

    reorganize UI components, add footer
    Former-commit-id: 0ee3f146396eb6f9260bb06df642c4f59e8a71aa

---
+++ view/src/vue/components/ui/footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer>
+    <img src="/img/brand/logo-white-outline.svg" alt="Nexus-Stats" class="ico-36">
+    <div class="resources">
+      <router-link to="">Contact</router-link>
+      <router-link to="">How do we get our data?</router-link>
+      <router-link to="">Developers</router-link>
+      <router-link to="">Privacy Policy</router-link>
+      <router-link to="">Imprint</router-link>
+      <router-link to="">Sitemap</router-link>
+    </div>
+  </footer>
+</template>
+
+<style lang='scss' scoped>
+  @import '~src/styles/partials/importer';
+
+  footer {
+    margin-top: auto;
+    text-align: center;
+    padding: 20px;
+    background: $colorBackgroundDarker;
+
+    img {
+      margin-top: 20px;
+      opacity: 0.4;
+    }
+
+    .resources {
+      max-width: $max-width;
+      padding: 20px 0;
+      margin: auto;
+
+      a {
+        color: $colorFontSubtle !important;
+        font-weight: 300;
+        padding: 0 10px;
+        border-right: 1px solid $colorSubtle;
+
+        &:hover {
+          color: white !important;
+        }
+
+        &:last-of-type {
+          border-right: none;
+        }
+      }
+    }
+  }
+</style>
