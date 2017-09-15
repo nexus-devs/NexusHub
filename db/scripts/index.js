72a85d4c77639824991ddc7c7fdb1daeef9ece5e
Kilian <kilian.volb@protonmail.com>
2017-09-16 01:36:03 +0200

    add error handlers
    Former-commit-id: 1f6eae9bfa17236ff1dbcab5c569bfec5492393d

--- db/scripts/index.js
+++ db/scripts/index.js
@@ -1,2 +1,9 @@
+process.on('uncaughtException', err => {
+  console.log(err)
+})
+process.on('unhandledRejection', err => {
+  console.log(err)
+})
+
 const scraper = require("./scraper.js")
 scraper.getItems()
