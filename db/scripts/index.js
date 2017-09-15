169c301656e62b47edef17a4253300bd46e10460
Kilian <kilian.volb@protonmail.com>
2017-09-16 01:55:26 +0200

    remove global error handler

--- db/scripts/index.js
+++ db/scripts/index.js
@@ -1,9 +1,2 @@
-process.on('uncaughtException', err => {
-  console.log(err)
-})
-process.on('unhandledRejection', err => {
-  console.log(err)
-})
-
 const scraper = require("./scraper.js")
 scraper.getItems()
