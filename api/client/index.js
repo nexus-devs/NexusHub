2c8f3ef8f9cc40b21169045d7d2f3cee3cb77667
mads-jakobsen <mads.strandbygaard.jakobsen@gmail.com>
2017-10-05 09:05:13 +0200

    fix index redirect

---
+++ api/client/index.js
@@ -0,0 +1,17 @@
+const Endpoint = require(blitz.config[blitz.id].endpointParent)
+
+/**
+ * Contains multi-purpose functions for child-methods and provides default values
+ */
+class Foo extends Endpoint {
+  constructor(api, db, url) {
+    super(api, db, url)
+    this.schema.url = '/'
+  }
+
+  async main(req, res) {
+    res.send('Documentation can be found at https://drive.google.com/open?id=16rbyQAG1cgQhwfFfXcHqn-o8txZ5dAZBf4hzr3VeJJE. I\'m too busy to hook a web server for docs right now. Sorry :>')
+  }
+}
+
+module.exports = Foo
