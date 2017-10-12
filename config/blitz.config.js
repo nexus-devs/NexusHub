e78b320d8720986470fc1de0a66804615f1165db
mads-jakobsen <mads.strandbygaard.jakobsen@gmail.com>
2017-10-12 02:06:52 +0200

    connect to live api with default user

--- config/blitz.config.js
+++ config/blitz.config.js
@@ -17,6 +17,12 @@ module.exports = {
       endpointPath: __dirname + '/../view/endpoints',
       sourcePath: __dirname + '/../view',
       publicPath: __dirname + '/../assets'
+    },
+    client: {
+      api: 'https://api.nexus-stats.com',
+      auth: 'https://auth.nexus-stats.com',
+      user_key: 'UaGcdduMzAdMmuML9Sk45epsaxjh74x1B97qdzYkgBfI9CDaZFoYAhu5YPA6w982',
+      user_secret: 'OHCufWlBHM4izpfBcZB3HYN2IqBrFpBk8Z1xjEhlQ6VeKLgQ0pX03TjQmHNoIYEI'
     }
   }
 }
