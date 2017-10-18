49d7c2b22c7473e99eda5f9896582123dacf01db
Kilian <kilian.volb@protonmail.com>
2017-10-18 21:09:05 +0200

    add config check for item list hook
    Former-commit-id: e4e3d6b5fb2b7efb7a3d4947867ecaecf4225ea4

--- hooks/db.js
+++ hooks/db.js
@@ -3,7 +3,8 @@ const items = require('../db/data/items.json')
 
 class Hook {
   async verifyItemList() {
-    const db = await mongodb.connect(blitz.config.client_worker.mongoURL || blitz.config.core_worker.mongoURL)
+    const url = blitz.config.client_worker ? blitz.config.client_worker.mongoURL : blitz.config.core_worker.mongoURL
+    const db = await mongodb.connect(url)
     items.forEach(item => {
       db.collection('items').update({
         name: item.name
