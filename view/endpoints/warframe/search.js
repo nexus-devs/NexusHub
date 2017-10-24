54cc00654e7a8896fbfbf3fe4acf0216d699030c
Kilian <kilian.volb@protonmail.com>
2017-10-24 11:13:12 +0200

    add search page routing

---
+++ view/endpoints/warframe/search.js
@@ -0,0 +1,11 @@
+const Endpoint = require(blitz.config[blitz.id].endpointParent)
+
+class Index extends Endpoint {
+  constructor(api, db, url) {
+    super(api, db, url)
+    this.schema.url = "/warframe/search/:query"
+    this.schema.view = "pages/warframe/search.vue"
+  }
+}
+
+module.exports = Index
