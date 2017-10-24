195c56ec008eeb09d6fe6add1814c0c65002252e
Kilian <kilian.volb@protonmail.com>
2017-10-24 11:13:12 +0200

    add search page routing
    Former-commit-id: 54cc00654e7a8896fbfbf3fe4acf0216d699030c

---
+++ view/pages/warframe/search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <navigation></navigation>
+    <sidebar>
+      <sidebar-search></sidebar-search>
+    </sidebar>
+  </div>
+</template>
+
+<script>
+import appContent from 'src/app-content.vue'
+import sidebar from 'src/components/ui/sidebar.vue'
+import sidebarSearch from 'src/components/ui/sidebar/search.vue'
+import navigation from 'src/components/ui/nav.vue'
+
+export default {
+  components: {
+    'app-content': appContent,
+    sidebar,
+    'sidebar-search': sidebarSearch,
+    navigation
+  },
+  mounted() {
+    console.log(this.$route.params)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
