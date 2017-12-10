9aa4e55d98a6f029f5888864fe945a74826945e7
Kaptard <kilian@nexus-stats.com>
2017-12-10 19:31:41 +0100

    code style

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const mongo = require('./hooks/mongo')
 const db = require('./hooks/db')
 const Core = require('blitz-js-core')
 
-/**
- * Load blitz-js framework
- */
+// Load blitz-js framework
 const Blitz = require('blitz-js')(config)
