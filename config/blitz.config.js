c23f134680c4d0cd6f4cdf5e8fb7bc5a92549376
Kaptard <kilian@nexus-stats.com>
2018-01-07 05:51:48 +0100

    restore non-docker config
    Former-commit-id: 12f8408a8ea54feb292191438569c04b08b6cdc7

--- config/blitz.config.js
+++ config/blitz.config.js
@@ -1,55 +1,31 @@
-/**
- * NOTE: All paths are relative to the nexus-stats repo config folder
- */
-const fs = require('fs')
-const certPrivate = fs.readFileSync(`/run/secrets/nexus-private-key`, 'utf-8')
-const certPublic = fs.readFileSync(`/run/secrets/nexus-public-key`, 'utf-8')
 const mongo = require('../hooks/mongo')
 const db = require('../hooks/db')
-const dbSecret = fs.readFileSync(`/run/secrets/mongo-admin-pwd`, 'utf-8').replace(/(\n|\r)+$/, '')
-const mongoUrl = `mongodb://admin:${dbSecret}@mongo/admin?replicaSet=nexus`
-const redisUrl = 'redis://redis'
+const mongoUrl = 'mongodb://localhost/'
 
 module.exports = {
   blitz: {
-    logLevel: 'verbose',
+    logLevel: 'monitor',
     environment: 'development'
-  },
-  api: {
-    redisUrl,
-    certPublic
   },
   core: {
     endpointPath: __dirname + '/../api',
     mongoUrl,
     mongoDb: 'nexus-core-warframe',
-    redisUrl,
     hooks: [mongo.verifyItemIndices, db.verifyItemList]
   },
   auth: {
-    api: {
-      redisUrl,
-      certPublic,
-    },
     core: {
       mongoUrl,
-      mongoDb: 'nexus-auth',
-      redisUrl,
-    },
-    certPrivate,
+      mongoDb: 'nexus-auth'
+    }
   },
   view: {
-    api: {
-      redisUrl,
-      certPublic
-    },
     core: {
       mongoUrl,
       mongoDb: 'nexus-view',
-      redisUrl,
       endpointPath: __dirname + '/../view/endpoints',
       sourcePath: __dirname + '/../view',
-      publicPath: __dirname + '/../assets'
+      publicPath: __dirname + '/../assets',
     },
     client: {
       apiUrl: 'https://api.nexus-stats.com',
