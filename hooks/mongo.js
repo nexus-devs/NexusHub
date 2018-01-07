a0277292a55a13734b0f401abeb34ecd71f282e1
Kaptard <kilian@nexus-stats.com>
2018-01-07 04:18:00 +0100

    adjust to mongo driver 3.x changes

--- hooks/mongo.js
+++ hooks/mongo.js
@@ -1,69 +1,31 @@
 const mongodb = require('mongodb').MongoClient
 
-module.exports = {
-
-  /**
-   * Ensures mongodb indexes and creates missing ones
-   */
-  verifyItemIndices: () => {
-    mongodb.connect(blitz.config.core.mongoUrl, function(err, db) {
-      if (err) throw err
-
-      // TODO: Change item index to text
-      blitz.log.verbose("Core      | verifying request indices")
-
-      // Player profiles
-      mongoVerifySingleIndex(db, 'players', {
-        name: 1
-      })
-
-      // Item stats accumulation
-      mongoVerifySingleIndex(db, 'requests', {
-        item: 1,
-        createdAt: 1
-      })
-      mongoVerifySingleIndex(db, 'requests', {
-        item: 1,
-        createdAt: 1,
-        region: 1,
-        rank: 1,
-      })
-    })
-  }
-}
-
 /**
  * Verifies a single index
- * @param {db} db - mongoDB
- * @param {string} col - Collection to verify
- * @param {object} index - Indices to verify
  */
 function mongoVerifySingleIndex(db, col, index) {
-  // Verify index
-  db.collection(col).createIndex(index)
+  db.db(blitz.config.core.mongoDb).collection(col).createIndex(index)
+}
 
-  // Verbose log string
-  let str = "Core      | verified "
 
-  // Get obj length
-  let objLength = Object.keys(index).map(key => index.hasOwnProperty(key)).length
+/**
+ * Ensures mongodb indexes for items, requests, players, etc
+ */
+module.exports = {
+  verifyItemIndices: async () => {
+    blitz.log.verbose("Core      | verifying warframe indices")
+    const db = await mongodb.connect(blitz.config.core.mongoUrl)
 
-  // Append possible compound
-  if (objLength > 1) {
-    str += "compound "
-  }
-
-  // Append names
-  let i = 0
-  for (let key in index) {
-    if (index.hasOwnProperty(key)) {
-      str += key
-      if (i < objLength - 1) str += "/"
-      i++
-    }
-  }
-
-  // Log
-  str += " index"
-  blitz.log.silly(str)
+    // Item stats accumulation
+    mongoVerifySingleIndex(db, 'requests', {
+      item: 1,
+      createdAt: 1
+    })
+    mongoVerifySingleIndex(db, 'requests', {
+      item: 1,
+      createdAt: 1,
+      region: 1,
+      rank: 1,
+    })
+  })
 }
