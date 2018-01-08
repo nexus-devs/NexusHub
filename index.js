d242c129ccc59c6cc9ce584293c9c49996a1d665
Kaptard <kilian@nexus-stats.com>
2018-01-08 23:22:46 +0100

    remove redundant libs from index
    Former-commit-id: 154305db18b2a3012d9b5c31cafcbdc8c0a5e928

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const config = require('./config/blitz.config.js')
-const mongo = require('./hooks/mongo')
-const db = require('./hooks/db')
-const Core = require('blitz-js-core')
 
 // Load blitz-js framework
 const Blitz = require('blitz-js')(config)
