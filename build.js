0d83daad630368345be74de5b3341ba9bd99d28f
Kaptard <kilian@nexus-stats.com>
2018-01-19 10:35:19 +0100

    add manual build script - don't rebuild on start

---
+++ build.js
@@ -0,0 +1,47 @@
+const util = require('util')
+const webpack = require('webpack')
+const rm = require('rimraf')
+
+// Imitate blitz config object for pulling default webpack config
+global.blitz = {
+  config: {
+    local: {
+      environment: 'production'
+    },
+    view: {
+      webpack: {
+        clientConfig: `${__dirname}/node_modules/blitz-js-view/config/webpack/client.config.js`,
+        serverConfig: `${__dirname}/node_modules/blitz-js-view/config/webpack/server.config.js`
+      },
+      core: {
+        publicPath: `${__dirname}/assets`,
+        sourcePath: `${__dirname}/view`
+      },
+      client: {
+        apiUrl: 'https://api.nexus-stats.com',
+        authUrl: 'https://auth.nexus-stats.com'
+      }
+    }
+  }
+}
+
+// Call webpack build function
+async function build() {
+  const timer = new Date
+  const clientConfig = require(blitz.config.view.webpack.clientConfig)
+  const serverConfig = require(blitz.config.view.webpack.serverConfig)
+
+  console.log('* Removing old builds...')
+  rm.sync(`${__dirname}/assets/*.bundle.*.js`)
+  rm.sync(`${__dirname}/assets/client.*.css`)
+
+  console.log('* Starting webpack build process. This might take a while...')
+  const compiled = await util.promisify(webpack)([clientConfig, serverConfig])
+
+  if (compiled.errors) {
+    throw compiled.errors
+  } else {
+    console.log(`> Webpack build successful (${new Date - timer}ms)`)
+  }
+}
+build()
