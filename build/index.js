83f8d187e653d854677c41edd17876e586e30cbb
Kaptard <kilian@nexus-stats.com>
2018-02-03 03:38:55 +0100

    add condition to only rebuild webpack when relevant files changed
    Former-commit-id: 24895c6fe1cf2909a56f13442bba6b15f379e964

--- build/index.js
+++ build/index.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = 'production'
 const util = require('util')
 const webpack = require('webpack')
 const rm = require('rimraf')
+const gitlog = require('gitlog')
+
 
 // Imitate blitz config object for pulling default webpack config
 global.blitz = {
@@ -28,13 +30,30 @@ global.blitz = {
 
 // Call webpack build function
 async function build() {
+  const changes = await util.promisify(gitlog)({
+    repo: __dirname + '/../',
+    number: 1
+  })
   const clientConfig = require(blitz.config.view.webpack.clientConfig)
   const serverConfig = require(blitz.config.view.webpack.serverConfig)
 
+  // Only rebuild webpack if files in view folder or dependencies have changed
+  let changed = false
+  changes[0].files.forEach(file => {
+    if (file.includes('view/') || file.includes('package.json')) {
+      changed = true
+    }
+  })
+  if (!changed) {
+    return
+  }
+
+  // Only keep one build at a time
   console.log('* Removing old builds...')
   rm.sync(`${__dirname}/../assets/*.bundle.*.js`)
   rm.sync(`${__dirname}/../assets/client.*.css`)
 
+  // Trigger webpack build
   console.log('* Starting webpack build process. This might take a while...')
   const timer = new Date
   const compiled = await util.promisify(webpack)([clientConfig, serverConfig])
