9c6132954f77a6c35df6bf167d54ed8b06b2b46c
Kaptard <kilian@nexus-stats.com>
2018-02-11 19:19:34 +0100

    feat: add automatic changelog generator
    Former-commit-id: b7b3a9cedc73c441100f8f71bc80a79b438fb0f4

---
+++ db/changelogs/data/changes.js
@@ -0,0 +1,28 @@
+changes: {
+  feat: {
+    scope1: [], // scopes generated from what's available
+    scope2: [{
+      commit: {
+        subject: '',
+        description: '',
+        url: '',
+      },
+      issues: [{
+        id: '',
+        title: '',
+        url: ''
+      }],
+      author: [{
+        name: '',
+        url: ''
+      }]
+    }],
+    api: [],
+    ui: []
+  },
+  fix: {
+    api: [],
+    ui: []
+  },
+  perf: {} // same as above
+}
