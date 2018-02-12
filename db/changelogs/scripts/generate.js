bba12f9313c5aad132c109097e66c3d77d758ecf
Kaptard <kilian@nexus-stats.com>
2018-02-12 02:39:43 +0100

    finish up changelog feature

--- db/changelogs/scripts/generate.js
+++ db/changelogs/scripts/generate.js
@@ -13,19 +13,74 @@ process.on('unhandledRejection', err => {
 class Logger {
   constructor() {
     this.logs = require('../data/logs.json')
+    this.types = ['feat', 'fix', 'perf']
   }
 
   async generate() {
     const commits = await this.getUnreleasedCommits()
-    const changes = []
+    const changes = {}
 
     commits.forEach(commit => {
-      console.log(commit)
-      commit.subject = commit.subject.split(' -')
-      console.log(commit.subject)
-    })
+      let message = commit.body.split('\n\n')
+      let scopere = commit.subject.match(/\(([^)]+)\)/)
+      commit.body = message[0]
+      commit.footer = message[1]
+      commit.scope = scopere ? scopere[1] : null
+      commit.type = commit.subject.split(': ')[0].replace(`(${commit.scope})`, '')
+      commit.subject = commit.subject.split(': ')[1]
 
-    return this.save(unreleased)
+      if (this.types.includes(commit.type)) {
+        let issues = this.getIssues(commit.footer)
+        let change = this.getChange(commit, issues)
+
+        // Ensure we have an object for commit type
+        let type = commit.type
+        changes[type] = changes[type] || {}
+
+        // Ensure scope exists within type
+        let scope = [commit.scope || 'general']
+        changes[type][scope] = changes[type][scope] || []
+
+        // Add change to target scope within target type
+        changes[type][scope].push(change)
+      }
+    })
+    return this.save(changes)
+  }
+
+  /**
+   * Get proper change format from a specified commit
+   */
+  getChange(commit, issues) {
+    return {
+      commit: {
+        subject: commit.subject,
+        description: commit.body,
+        url: `https://github.com/nexus-devs/nexus-stats/commit/${commit.hash}`,
+      },
+      issues,
+      author: {
+        name: commit.authorName,
+        url: `https://github.com/${commit.authorName}`
+      }
+    }
+  }
+
+  /**
+   * Find related issues in commit footer
+   */
+  getIssues(footer) {
+    let issues = []
+
+    if (footer) {
+      footer.match(/[0-9]*/g).forEach(num => {
+        if (num) issues.push({
+          id: `#${num}`,
+          url: `https://github.com/nexus-devs/nexus-stats/issues/${num}`
+        })
+      })
+    }
+    return issues
   }
 
   /**
@@ -37,8 +92,8 @@ class Logger {
       tag: {},
       release: {}
     }
-    this.latest.tag.id = (await exec('git rev-list --tags --max-count=1')).stdout
-    this.latest.tag.name = (await exec(`git describe --tags ${this.latest.tag.id}`)).stdout
+    this.latest.tag.id = (await exec('git rev-list --tags --max-count=1')).stdout.replace(/\n$/, '')
+    this.latest.tag.name = (await exec(`git describe --tags ${this.latest.tag.id}`)).stdout.replace(/\n$/, '')
     this.latest.release = this.logs.find(e => e.release.name === this.latest.tag.name)
     this.latest.release = this.latest.release ? this.latest.release.release : null
 
@@ -47,7 +102,7 @@ class Logger {
     if (!this.latest.release && this.logs[0]) {
       const unreleased = _.cloneDeep(this.logs[0])
       this.logs[0].release = this.latest.release = {
-        name: this.latest.tag.name.replace(/\n$/, ''),
+        name: this.latest.tag.name,
         id: this.latest.tag.id,
         date: unreleased.release.date
       }
@@ -56,20 +111,28 @@ class Logger {
     return await promisify(gitlog)({
       repo: `${__dirname}/../../../`,
       since: new Date(this.latest.release.date),
-      fields: ['subject', 'body']
+      fields: ['hash', 'subject', 'body', 'authorName', 'authorDate']
     })
   }
 
   /**
    * Save formatted unreleased changes to this.logs and save to file
    */
-  async save(logs) {
-    if (this.logs[0] && this.logs[0].release === 'unreleased') {
-      this.logs[0] = logs
+  async save(changes) {
+    let unreleased = {
+      release: {
+        name: 'unreleased',
+        id: null,
+        date: new Date().getTime()
+      },
+      changes
+    }
+    if (this.logs[0] && this.logs[0].release.name === 'unreleased') {
+      this.logs[0] = unreleased
     }
     // Either no logs present or new tag prepended
     else {
-      this.logs.unshift(logs)
+      this.logs.unshift(unreleased)
     }
     return promisify(fs.writeFile)(`${__dirname}/../data/logs.json`, JSON.stringify(this.logs, null, 2))
   }
