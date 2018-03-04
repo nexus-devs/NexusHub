cbd50eecd976fd81d3331897b0e30a2630f07338
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-03-04 06:28:08 +0100

    refactor: move individual node config back into /config, use /ui instead of /view

--- index.js
+++ index.js
@@ -4,35 +4,28 @@ const Core = require('blitz-js-core')
 const Auth = require('blitz-js-auth')
 const View = require('blitz-js-view')
 const wfhooks = require('./hooks/warframe.js')
+const config = {
+  blitz: require('./config/blitz/blitz.js'),
+  auth: require('./config/blitz/auth.js'),
+  view: require('./config/blitz/view.js'),
+  warframe: require('./config/blitz/warframe.js')
+}
+const blitz = new Blitz(config.blitz)
 
 /**
- * Load nodes with custom config
+ * Auth server for verifying users
  */
-const blitz = new Blitz({
-  logLevel: 'info'
-})
-blitz.use(new Auth({
-  core: {
-    mongoDb: 'nexus-auth'
-  }
-}))
-blitz.use(new View({
-  client: {
-    apiUrl: 'https://api.nexus-stats.com',
-    authUrl: 'https://auth.nexus-stats.com'
-  },
-  core: {
-    mongoDb: 'nexus-view',
-  }
-}))
+blitz.use(new Auth(config.auth))
+
+/**
+ * View server for providing the web client
+ */
+blitz.use(new View(config.view))
+
+/**
+ * Warframe API setup
+ */
 blitz.hook('warframe.core', wfhooks.verifyIndices)
 blitz.hook('warframe.core', wfhooks.verifyItemList)
-blitz.use(new Api({
-  group: 'warframe'
-}))
-blitz.use(new Core({
-  endpointPath: __dirname +'/api/warframe',
-  baseUrl: '/warframe',
-  mongoDb: 'nexus-warframe',
-  group: 'warframe'
-}))
+blitz.use(new Api(config.warframe.api))
+blitz.use(new Core(config.warframe.core))
