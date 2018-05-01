c0f5754c5bf66bbbb4b0c5efff269165accaad96
Kilian <kilian@nexus-stats.com>
2018-05-01 21:16:54 +0200

    feat(config): Implement config for docker environment
    Former-commit-id: f0319b6b154dde48062ec622d4df3c1109290dc2

--- config/cubic/auth.js
+++ config/cubic/auth.js
@@ -1,5 +1,31 @@
-module.exports = {
-  core: {
-    mongoDb: 'nexus-auth'
+// Use some adaptions when inside docker, especially database connections.
+if (process.env.DOCKER) {
+  const fs = require('fs')
+  const certPrivate = fs.readFileSync(`/run/secrets/nexus-private-key`, 'utf-8')
+  const certPublic = fs.readFileSync(`/run/secrets/nexus-public-key`, 'utf-8')
+  const dbSecret = fs.readFileSync(`/run/secrets/mongo-admin-pwd`, 'utf-8').replace(/(\n|\r)+$/, '')
+  const mongoUrl = `mongodb://admin:${dbSecret}@mongo/admin?replicaSet=nexus`
+  const redisUrl = 'redis://redis'
+
+  module.exports = {
+    api: {
+      redisUrl,
+      certPublic
+    },
+    core: {
+      mongoUrl,
+      mongoDb: 'nexus-auth',
+      redisUrl
+    },
+    certPrivate
+  }
+}
+
+// Normal environment should be fine with default config.
+else {
+  module.exports = {
+    core: {
+      mongoDb: 'nexus-auth'
+    }
   }
 }
--- config/cubic/main.js
+++ config/cubic/main.js
@@ -1,11 +1,39 @@
-module.exports = {
-  api: {
-    group: 'main'
-  },
-  core: {
-    endpointPath: `${process.cwd()}/api/`,
-    endpointPathExclude: /\/warframe/,
-    mongoDb: 'nexus-main',
-    group: 'main'
+// Use some adaptions when inside docker, especially database connections.
+if (process.env.DOCKER) {
+  const fs = require('fs')
+  const certPublic = fs.readFileSync(`/run/secrets/nexus-public-key`, 'utf-8')
+  const dbSecret = fs.readFileSync(`/run/secrets/mongo-admin-pwd`, 'utf-8').replace(/(\n|\r)+$/, '')
+  const mongoUrl = `mongodb://admin:${dbSecret}@mongo/admin?replicaSet=nexus`
+  const redisUrl = 'redis://redis'
+
+  module.exports = {
+    api: {
+      redisUrl,
+      certPublic,
+      group: 'main'
+    },
+    core: {
+      mongoUrl,
+      mongoDb: 'nexus-main',
+      redisUrl,
+      endpointPath: `${process.cwd()}/api/`,
+      endpointPathExclude: /\/warframe/,
+      group: 'main'
+    }
+  }
+}
+
+// Normal environment should be fine with default config.
+else {
+  module.exports = {
+    api: {
+      group: 'main'
+    },
+    core: {
+      endpointPath: `${process.cwd()}/api/`,
+      endpointPathExclude: /\/warframe/,
+      mongoDb: 'nexus-main',
+      group: 'main'
+    }
   }
 }
