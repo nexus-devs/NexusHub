b39e0be0434597ab19a4a3a3d0c6a51b08c843aa
Kilian <kilian@nexus-stats.com>
2018-05-02 03:39:51 +0200

    fix(docker): Fix docker deploy script.
    Former-commit-id: 4d00fdb67f3c12cd4065aed4efc55c0435721dd5

--- docker.sh
+++ docker.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
-devpath="$( cd "$(dirname "$0")" ; pwd -P )"
+#!/bin/bash
+# Bind current directory to docker container
+devpath="$( cd "$(dirname "$0")" ; pwd -P )"'/'
+
+# If directory starts with /mnt/, replace that with /, so the full path looks
+# like /c/path/to/repo. Bind mounts won't work otherwise on bash on windows.
+devpath="$(echo $devpath | sed -e 's/^\/mnt\//\//')"
+
+# Get docker repo and deploy
 if [ ! -d "./docker" ]; then
   git clone https://github.com/nexus-devs/docker
 fi
-
-bash docker/init-swarm.sh $@ $devpath
+bash -c "cd ./docker && exec bash deploy.sh $@ $devpath"
