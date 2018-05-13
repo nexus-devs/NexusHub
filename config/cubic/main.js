b51f9dc01da1f537a8d4bfa30b089a6a41dea320
Kilian <kilian@nexus-stats.com>
2018-05-13 13:42:33 +0200

    fix: Use .trim() to remove whitespace like a normal human being.
    Former-commit-id: 2c320e0b8f31fe4e91532156589df802f3240c92

--- config/cubic/main.js
+++ config/cubic/main.js
@@ -6,7 +6,7 @@ const group = node ? node.split('-')[0] : null // auth-core, ui-core, etc, take 
 if (process.env.DOCKER && (prod ? group === 'main' : true)) {
   const fs = require('fs')
   const certPublic = fs.readFileSync(`/run/secrets/nexus-public-key`, 'utf-8')
-  const dbSecret = fs.readFileSync(`/run/secrets/mongo-admin-pwd`, 'utf-8').replace(/(\n|\r)+$/, '')
+  const dbSecret = fs.readFileSync(`/run/secrets/mongo-admin-pwd`, 'utf-8').trim()
   const mongoUrl = `mongodb://admin:${dbSecret}@mongo/admin?replicaSet=nexus`
   const redisUrl = 'redis://redis'
   const config = {
@@ -27,8 +27,8 @@ if (process.env.DOCKER && (prod ? group === 'main' : true)) {
   if (prod) {
     config.core.apiUrl = 'http://main_api:3003'
     config.core.authUrl = 'http://auth_api:3030'
-    config.core.userKey = fs.readFileSync('/run/secrets/nexus-main-key', 'utf-8').replace(/(\n|\r)+$/, '')
-    config.core.userSecret = fs.readFileSync('/run/secrets/nexus-main-secret', 'utf-8').replace(/(\n|\r)+$/, '')
+    config.core.userKey = fs.readFileSync('/run/secrets/nexus-main-key', 'utf-8').trim()
+    config.core.userSecret = fs.readFileSync('/run/secrets/nexus-main-secret', 'utf-8').trim()
   }
   module.exports = config
 }
