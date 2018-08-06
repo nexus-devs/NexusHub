f3cbc30363e152d6a3c62e47e3b476a8ddf70927
Kilian <kilian@nexus-stats.com>
2018-08-06 17:06:47 +0200

    feat: Add optional debug flag for docker environment.
    Sets log level to silly, which is useful when shit doesn't work and you 
    don't know why.
    
    
    Former-commit-id: e22ad687e7ac96757874031ebee20984a92ee15f

--- config/cubic/cubic.js
+++ config/cubic/cubic.js
@@ -1,8 +1,9 @@
 const prod = process.env.NODE_ENV === 'production'
+const debug = process.env.NODE_DEBUG
 const docker = process.env.DOCKER
 
 module.exports = {
-  logLevel: prod ? 'monitor' : 'info',
+  logLevel: debug ? 'silly' : (prod ? 'monitor' : 'info'),
   skipAuthCheck: prod && docker,
   throwErrors: !prod
 }
