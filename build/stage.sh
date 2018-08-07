0ba87160a7949f5e322923e2d2c2ebf26cfc7a63
Kilian <kilian@nexus-stats.com>
2018-08-07 13:31:01 +0200

    ci: Force full copy of dev branch when staging.
    Former-commit-id: e200d06a4064840480f1e39a5b1e15e7a0b41a0c

---
+++ build/stage.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# If new builds were added - Force merge with staging so we won't have duplicate
+# builds. Otherwise, just do a normal merge.
+if [ "$(cat config/webpack/build.json | grep enable)" ]; then
+  echo '* New webpack builds found - Override staging.'
+  git push 'https://nexus-ci:'$NEXUS_CI_TOKEN'@github.com/nexus-devs/nexus-stats' --force development:staging 2>/dev/null
+else
+  echo '* No new webpack builds - Merge recursively.'
+  git checkout development
+  git checkout staging
+  git merge -s recursive -X theirs development
+  git push 'https://nexus-ci:'$NEXUS_CI_TOKEN'@github.com/nexus-devs/nexus-stats' staging 2>/dev/null
+fi
+# ^ suppress output to /dev/null to keep secrets hidden
