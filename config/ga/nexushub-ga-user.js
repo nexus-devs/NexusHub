f826c751fc11b761f95228b5fb3d70acfa5baca1
Kilian <kilian@nexus-stats.com>
2018-08-09 21:11:39 +0200

    fix(docker): Fix google analytics config not actually reading GA cert.
    Former-commit-id: dd83d8dbde8c966e92d5ad5ea20f42a6affc1f73

--- config/ga/nexushub-ga-user.js
+++ config/ga/nexushub-ga-user.js
@@ -1,8 +1,9 @@
-const prod = process.env.NODE_ENV === 'production'
+const docker = process.env.NODE_ENV === 'production' && process.env.DOCKER
+const fs = require('fs')
 
 module.exports = {
   email: 'company-page@nexushub-ga.iam.gserviceaccount.com',
   clientId: '116601860147515971555',
-  key: prod ? '/run/secrets/nexushub-ga-key' : `${__dirname}/nexushub-ga-private.pem`,
+  key: docker ? fs.readFileSync('/run/secrets/nexushub-ga-key').trim() : `${__dirname}/nexushub-ga-private.pem`,
   ids: 'ga:126211225'
 }
