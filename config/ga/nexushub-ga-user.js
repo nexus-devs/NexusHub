a5f93714d6904c8cd9884738032dac7f1f2f9014
Kilian <kilian@nexus-stats.com>
2018-08-09 15:46:28 +0200

    fix(docker): Support docker secret for GA key

--- config/ga/nexushub-ga-user.js
+++ config/ga/nexushub-ga-user.js
@@ -1,6 +1,8 @@
+const prod = process.env.NODE_ENV === 'production'
+
 module.exports = {
   email: 'company-page@nexushub-ga.iam.gserviceaccount.com',
   clientId: '116601860147515971555',
-  key: `${__dirname}/nexushub-ga-private.pem`,
+  key: prod ? '/run/secrets/nexushub-ga-key' : `${__dirname}/nexushub-ga-private.pem`,
   ids: 'ga:126211225'
 }
