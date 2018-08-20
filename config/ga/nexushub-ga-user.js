8b0a02a156a327fef39d4bd1b6700797cf94a22d
Kilian <kilian@nexus-stats.com>
2018-08-20 23:02:36 +0200

    fix(ga): Fix key location for docker.

--- config/ga/nexushub-ga-user.js
+++ config/ga/nexushub-ga-user.js
@@ -1,9 +1,8 @@
 const docker = process.env.NODE_ENV === 'production' && process.env.DOCKER
-const fs = require('fs')
 
 module.exports = {
   email: 'company-page@nexushub-ga.iam.gserviceaccount.com',
   clientId: '116601860147515971555',
-  key: docker ? fs.readFileSync('/run/secrets/nexus-ga-key', 'utf-8') : `${__dirname}/nexushub-ga-private.pem`,
+  key: docker ? '/run/secrets/nexus-ga-key' : `${__dirname}/nexushub-ga-private.pem`,
   ids: 'ga:126211225'
 }
