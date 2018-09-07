374022344caf76e0edbb24b0954071dc3a73ef11
Nakroma <tarackobarack@gmail.com>
2018-09-07 17:14:47 +0200

    added basic result doc generation
    Former-commit-id: 1943a8f19ad8474b31bd180e980fff740daf44d8

--- api/warframe/v1/items/prices.js
+++ api/warframe/v1/items/prices.js
@@ -1,4 +1,5 @@
 const Endpoint = cubic.nodes.warframe.core.Endpoint
+const _ = require('lodash')
 const moment = require('moment')
 
 /**
@@ -45,9 +46,65 @@ class Prices extends Endpoint {
       return res.status(404).send(response)
     }
 
+    let doc = this.generateDocument(itemResult, timerange)
     this.getPrices(timeNow, timerange, item)
 
-    res.send({ 'yo': 'ya' })
+    res.send(doc)
+  }
+
+  /**
+   * Generates the response document
+   */
+  generateDocument (itemResult, timerange) {
+    let doc = { // Main document
+      name: itemResult.name,
+      components: []
+    }
+
+    this.generateComponents(doc, itemResult, timerange)
+    return doc
+  }
+
+  /**
+   * Generates the components to a corresponding item
+   */
+  generateComponents (doc, itemResult, timerange) {
+    itemResult.components.forEach(comp => {
+      const data = {
+        median: null,
+        min: null,
+        max: null,
+        offers: null,
+        days: []
+      }
+      const dataSection = {
+        current: _.cloneDeep(data),
+        previous: _.cloneDeep(data)
+      }
+      let component = {
+        name: comp.name,
+        buying: _.cloneDeep(dataSection),
+        selling: _.cloneDeep(dataSection),
+        combined: _.cloneDeep(dataSection)
+      }
+
+      // Fill component with intervals
+      const day = _.cloneDeep(data)
+      delete day.days
+
+      // TODO: Maybe put day pushing into own function?
+      for (let i = 0; i < timerange; i++) {
+        component.buying.current.days.push(_.cloneDeep(day))
+        component.buying.previous.days.push(_.cloneDeep(day))
+        component.selling.current.days.push(_.cloneDeep(day))
+        component.selling.previous.days.push(_.cloneDeep(day))
+        component.combined.current.days.push(_.cloneDeep(day))
+        component.combined.previous.days.push(_.cloneDeep(day))
+      }
+
+      // Add to result
+      doc.components.push(component)
+    })
   }
 
   /**
@@ -55,12 +112,17 @@ class Prices extends Endpoint {
    */
   async getPrices (timeNow, timerange, item) {
     // Get either pre-saved prices, or generate them if they don't exist
-    for (let i = 1; i <= timerange; i++) {
+    for (let i = 1; i <= timerange * 2; i++) {
       const dayCursor = timeNow.subtract(i, 'days').startOf('day')
       let cursorResult = await this.db.collection('items_presaves').findOne({
         name: new RegExp('^' + item + '$', 'i'),
         createdAt: dayCursor
       })
+      if (!cursorResult) {
+
+      } else {
+
+      }
     }
   }
 }
