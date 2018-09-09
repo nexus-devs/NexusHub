2279ac10aec91c7cd0ebb5d6131259c0749b8fa4
Nakroma <tarackobarack@gmail.com>
2018-09-09 19:54:58 +0200

    changed date copying to smth more readable
    Former-commit-id: b29a41bbbb5644f1bfad94a8eb207672dd6f7772

--- api/warframe/v1/items/prices.js
+++ api/warframe/v1/items/prices.js
@@ -113,7 +113,7 @@ class Prices extends Endpoint {
   async getPrices (doc, now, timerange, item) {
     // Get either pre-saved day prices, or generate them if they don't exist
     for (let i = 1; i < timerange * 2; i++) {
-      const dayCursor = moment(now).subtract(i, 'days').startOf('day')
+      const dayCursor = now.clone().subtract(i, 'days').startOf('day')
       let cursorResult = await this.db.collection('items_presaves').findOne({
         name: new RegExp('^' + item + '$', 'i'),
         createdAt: dayCursor
@@ -141,9 +141,9 @@ class Prices extends Endpoint {
     }
 
     // Get either pre-saved hour prices for the current day, or generate them if they don't exist
-    const startOfDay = moment(now).startOf('day')
-    for (let i = 0; moment(startOfDay).add(i, 'hours').isBefore(moment(now).startOf('hour')); i++) {
-      const hourCursor = moment(startOfDay).add(i, 'hours')
+    const startOfDay = now.clone().startOf('day')
+    for (let i = 0; startOfDay.clone().add(i, 'hours').isBefore(moment(now).startOf('hour')); i++) {
+      const hourCursor = startOfDay.clone().add(i, 'hours')
       let cursorResult = await this.db.collection('items_presaves').findOne({
         name: new RegExp('^' + item + '$', 'i'),
         createdAt: hourCursor
@@ -172,7 +172,7 @@ class Prices extends Endpoint {
     }
 
     let test = await this.db.collection('orderHistory').aggregate([
-      { $match: { 'createdAt': { $gte: moment(now).startOf('hour') } } },
+      { $match: { 'createdAt': { $gte: now.clone().startOf('hour') } } },
       { $group: { _id: '$offer', offers: { $sum: 1 }, min: { $min: '$price' }, max: { $max: '$price' } } }
     ]).toArray()
     console.log(test)
