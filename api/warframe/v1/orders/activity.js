50c272a8d927162a5f7effc43723a4aaeaaf2c26
Kilian <kilian@nexus-stats.com>
2018-09-27 11:34:48 +0200

    ci(docker): Update submodule

--- api/warframe/v1/orders/activity.js
+++ api/warframe/v1/orders/activity.js
@@ -1,20 +1,31 @@
 const Endpoint = cubic.nodes.warframe.core.Endpoint
+const Aggregator = require(`${process.cwd()}/api/lib/aggregator.js`)
+const _ = require('lodash')
+const moment = require('moment')
+const title = (str) => str.toLowerCase().replace(/\b\w/g, l => l.toUpperCase())
 
 class Activity extends Endpoint {
   constructor (api, db, url) {
     super(api, db, url)
-    this.schema.description = 'Trade activity, used for "busy hours".'
+    this.schema.description = 'Trade activity, used for "busy hours". Returned hours are in UTC, so shift the array depending on your offset.'
     this.schema.query = [
       {
         name: 'item',
         description: 'Item to reduce orders by. If no item is specified, the data for all items is returned.'
+      }, {
+        name: 'timerange',
+        default: 7,
+        description: 'Days from now to the last order.'
       }
     ]
+    this.schema.response = String
   }
 
+  /**
+   * Entrypoint for EndpointController
+   */
   async main (req, res) {
-    const item = req.query.item
-    
+    res.send('ok')
   }
 }
 
