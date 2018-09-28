d05645332e699ecf7484e447e4bcc2c93abe38b4
Kilian <kilian@nexus-stats.com>
2018-09-28 13:45:28 +0200

    ci: Fix issue where staging step won't detect builds.

--- build/stage.sh
+++ build/stage.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 # Commit builds from dev branch and go to staging
-webpack="$(cat config/webpack/build.json | grep '\"enable\": true')"
 git add -A
 git commit -m "Add staging builds"
+git checkout development
+git checkout staging
 
 # If new builds were added - Remove all old ones and pop the stash. Otherwise,
 # just pop the stash.
-if [ -z webpack ]; then
+if cat config/webpack/build.json | grep -q '\"enable\": true'; then
   echo '* New webpack builds found - Override staging.'
+  git checkout development
   git push 'https://nexus-ci:'$NEXUS_CI_TOKEN'@github.com/nexus-devs/nexus-stats' --force development:staging 2>/dev/null
 else
-  git checkout development
-  git checkout staging
+  echo '* No new webpack builds - Just merge.'
   git merge -s recursive -X theirs development
   git push 'https://nexus-ci:'$NEXUS_CI_TOKEN'@github.com/nexus-devs/nexus-stats' staging 2>/dev/null
 fi
