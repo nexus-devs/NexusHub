6d1b4688e26c86372addeaf81ef47bc267412bfa
Kilian <kilian@nexus-stats.com>
2018-09-28 20:16:53 +0200

    ci: Fix issue where staging step would always hard reset.

--- build/stage.sh
+++ build/stage.sh
@@ -3,13 +3,14 @@
 git add -A
 git commit -m "Add staging builds"
 git checkout staging
+git checkout development -- /config/webpack/build.json
 
 # If new builds were added - Remove all old ones and pop the stash. Otherwise,
 # just pop the stash.
 if cat config/webpack/build.json | grep -q '\"enable\": true'; then
   echo '* New webpack builds found - Override staging.'
   git reset --hard development
-  git merge -m "Reset staging to development." -s ours origin/staging
+  git merge -m "Merge development with staging." -s ours origin/staging
 else
   echo '* No new webpack builds - Just merge.'
   git merge -s recursive -X theirs development
