1e5cdf02ceafe90be686816922ba6f146d1786fe
Kilian <kilian@nexus-stats.com>
2018-10-08 21:15:59 +0200

    fix: Support credentials for API cubic-client

--- config/cubic/warframe.js
+++ config/cubic/warframe.js
@@ -20,10 +20,12 @@ if (process.env.DOCKER && (prod ? group === 'warframe' : true)) {
     }
   }
   if (prod) {
+    const userKey = fs.readFileSync('/run/secrets/nexus-warframe-key', 'utf-8').trim()
+    const userSecret = fs.readFileSync('/run/secrets/nexus-warframe-secret', 'utf-8').trim()
     config.core.apiUrl = 'ws://main_api:3003/ws'
     config.core.authUrl = 'ws://auth_api:3030/ws'
-    config.core.userKey = fs.readFileSync('/run/secrets/nexus-warframe-key', 'utf-8').trim()
-    config.core.userSecret = fs.readFileSync('/run/secrets/nexus-warframe-secret', 'utf-8').trim()
+    config.core.userKey = config.api.userKey = userKey
+    config.core.userSecret = config.api.userSecret = userSecret
   }
   module.exports = config
 }
