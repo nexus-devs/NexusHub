4e2d24d9e6c32b7fe0bde081c6e4a4feafd5e25c
Kilian <kilian@nexus-stats.com>
2018-10-13 18:24:30 +0200

    fix(bots): Don't override refresh token on node launch.

--- services/bootstrap/regular.js
+++ services/bootstrap/regular.js
@@ -40,8 +40,7 @@ module.exports = async function () {
       user_key: 'nexus-warframe-bot',
       user_secret: await bcrypt.hash('dev-only', 1),
       last_ip: [],
-      scope: 'write_orders_warframe',
-      refresh_token: null
+      scope: 'write_orders_warframe'
     }
   }, {
     upsert: true
