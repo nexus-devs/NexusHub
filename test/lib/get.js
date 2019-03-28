f0a2dc3e12fd0dcb69f391c32f3d298f51f4ee75
Kaptard <kilian@nexus-stats.com>
2019-03-28 12:29:28 +0100

    ci: Add conditional unit tests for webpack builds

---
+++ test/lib/get.js
@@ -0,0 +1,22 @@
+/**
+ * Helper function get http response as soon as server loaded
+ */
+const request = require('request-promise')
+let counter = 0
+
+module.exports = async function get (url, port = 3003) {
+  return new Promise(async resolve => {
+    try {
+      resolve(await request.get(`http://localhost:${port}${url}`))
+    } catch (err) {
+      counter++
+      if (counter > 20) {
+        console.error(err.message)
+        console.log('Retrying in 500ms...')
+      }
+      setTimeout(async () => {
+        resolve(await get(url, port))
+      }, 500)
+    }
+  })
+}
--- test/smoke/server.js
+++ test/smoke/server.js
@@ -1,9 +1,12 @@
 const Cubic = require('cubic')
 const Api = require('cubic-api')
 const Auth = require('cubic-auth')
+const Ui = require('cubic-ui')
 const Client = require('cubic-client')
+const get = require('../lib/get.js')
 const redisUrl = 'redis://redis'
 const mongoUrl = 'mongodb://mongodb'
+const webpack = require('../../config/webpack/build.json')
 const ci = process.env.DRONE
 const config = {
   cubic: require(`${process.cwd()}/config/cubic/cubic`),
@@ -30,6 +33,7 @@ before(async function () {
   const cubic = new Cubic(config.cubic)
   await cubic.use(new Auth(config.auth))
   await cubic.use(new Api(config.api))
+  await cubic.use(new Ui(config.ui))
 })
 
 /**
@@ -40,4 +44,11 @@ describe('Server', function () {
     const client = new Client()
     await client.connecting()
   })
+
+  // Check if webpack build works(only if rebuild was required)
+  if (webpack.enable) {
+    it('should load up UI node - GET /', async function () {
+      await get('/', 3000)
+    })
+  }
 })
