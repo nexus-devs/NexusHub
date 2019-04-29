b3772f557825f88b78515a0bfe7de7f0fafce5b2
Kaptard <kilian@nexus-stats.com>
2019-04-29 02:03:40 +0200

    refactor: Implement client based service for order tracker

--- services/warframe/clear.js
+++ services/warframe/clear.js
@@ -1,40 +1,12 @@
 /**
- * Clear Warframe's Order every few minutes.
+ * This service cleans the database from outdated warframe
+ * orders every few minutes. This script is supposed to run
+ * in a separate docker service.
  */
-const prod = process.env.NODE_ENV === 'production'
-const docker = process.env.DOCKER
-const staging = process.env.NEXUS_STAGING
-const getDevUser = require('../bootstrap/getDevUser.js')
-let userKey, userSecret, apiUrl, authUrl
-
+const getClient = require('../getClient.js')
 async function monitor () {
-  // On staging/prod build? Just read use the pre-generated credentials.
-  if (docker && prod) {
-    const fs = require('fs')
-    userKey = fs.readFileSync(`/run/secrets/nexus-cubic-key`, 'utf-8').trim()
-    userSecret = fs.readFileSync(`/run/secrets/nexus-cubic-secret`, 'utf-8').trim()
-    apiUrl = staging ? 'wss://api.staging.nexushub.co/ws' : 'wss://api.nexushub.co/ws'
-    authUrl = staging ? 'wss://auth.staging.nexushub.co/ws' : 'wss://auth.nexushub.co/ws'
-  }
+  const client = await getClient()
 
-  // In dev mode: Register new user if necessary and give it root perms
-  else {
-    const user = await getDevUser()
-    userKey = user.userKey
-    userSecret = user.userSecret
-  }
-
-  const config = {
-    api_url: apiUrl || 'ws://localhost:3003/ws',
-    auth_url: authUrl || 'ws://localhost:3030/ws',
-    user_key: userKey,
-    user_secret: userSecret
-  }
-  const Client = require('cubic-client')
-  const client = new Client(config)
-
-  // This repeats the cleaning process with a delay of 1 minute between
-  // clearing. This does not mean that it is cleared every minute.
   while (true) {
     await client.get('/warframe/v1/orders/clear')
     await new Promise(resolve => setTimeout(resolve, 1000 * 60))
--- services/warframe/tradechat.js
+++ services/warframe/tradechat.js
@@ -2,23 +2,18 @@
  * Dev-only service that listens to orders sent to the real API, just to have
  * some sample data when developing.
  */
+const getClient = require('../getClient.js')
+
 async function monitor () {
-  const endpoints = cubic.nodes.api.server.ws.endpoints
-  const db = (await endpoints.db).db(endpoints.config.mongoDb)
-  const options = { db, cache: endpoints.cache, ws: endpoints.ws }
-  const Order = require('../../api/warframe/v1/orders/new.js')
-  const order = new Order({ ...options, ...{ url: '/warframe/v1/orders' } })
   const Client = require('cubic-client')
   const client = new Client({
     api_url: 'wss://api.nexushub.co/ws',
     auth_url: 'wss://auth.nexushub.co/ws'
   })
+  const local = await getClient()
 
-  // Track orders from production API and post them locally. This includes wfm
-  // orders.
   client.subscribe('/warframe/v1/orders', req => {
-    const res = { send () {} }
-    order.main({ body: req }, res)
+    local.post('/warframe/v1/orders', req)
   })
 }
 monitor()
