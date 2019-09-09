386acaf1fef2c2c17b2dcc8d695add39cff2ca9f
Nakroma <tarackobarack@gmail.com>
2019-09-09 23:40:40 +0200

    added basic order parsing

---
+++ api/wow-classic/v1/new.js
@@ -0,0 +1,106 @@
+const Endpoint = require('cubic-api/endpoint')
+const moment = require('moment')
+
+class Scan extends Endpoint {
+  constructor (options) {
+    super(options)
+    this.schema.description = 'Parses a new scan into the database.'
+    this.schema.method = 'POST'
+    this.schema.url = '/wow-classic/v1/new'
+    this.schema.request = {
+      body: {
+        shortChar: 'Anjour',
+        testScan: false,
+        elapsed: 2.03867300000787,
+        data: 'i4571?1800!/2,1,1847,1945,',
+        itemDBcount: 4691,
+        realm: 'Transcendence',
+        ts: 1568041118,
+        itemsCount: 2614,
+        region: 'classic',
+        dataFormatVersion: 6,
+        char: 'Anjour-Transcendence',
+        newItems: 620,
+        count: 15027,
+        faction: 'Alliance',
+        dataFormatInfo: 'v4key!seller1/timeleft,itemCount,minBid,buyoutPrice,curBid&auction2&auction3!seller2/a1&a2 ...\n(grouped by itemKey, seller) with auction units being timeleft,itemCount,minBid,buyoutPrice,curBid',
+        firstCount: 15027,
+        classic: true
+      }
+    }
+    this.schema.response = String
+  }
+
+  async main (req, res) {
+    return res.send(this.parseData(req.body))
+  }
+
+  // Parse the AHDB data into something usable TODO: Put this in the client to relieve the server
+  parseData (body) {
+    const orders = []
+    const itemSets = body.data.split(' ')
+    const timestamp = moment.unix(body.ts) // Convert unix timestamp
+
+    for (const itemSet of itemSets) {
+      const itemOrders = itemSet.split('!')
+      const itemId = itemOrders.shift() // First value of the array is only the Id
+
+      for (const itemOrder of itemOrders) {
+        const itemOrderSplit = itemOrder.split('/')
+        const seller = itemOrderSplit[0]
+        const auctions = itemOrderSplit[1].split('&')
+
+        for (const auction of auctions) {
+          // Split auction data and map String to int, or null if empty
+          const auctionData = auction.split(',').map(elem => {
+            if (elem === '') return null
+            else return parseInt(elem)
+          })
+
+          // Since there is no exact creation time or Id,
+          // we need to calculate the possible time borders
+          // and make a guesstimate for scan merging with that.
+          let lowerDateBorder, upperDateBorder
+          switch (auctionData[0]) {
+            case 1: // Short, 0-30 min
+              lowerDateBorder = 0
+              upperDateBorder = 30
+              break
+            case 2: // Medium, 30m-2h
+              lowerDateBorder = 30
+              upperDateBorder = 120
+              break
+            case 3: // Long, 2h-8h
+              lowerDateBorder = 120
+              upperDateBorder = 480
+              break
+            case 4: // Very Long, 8h-24h
+              lowerDateBorder = 480
+              upperDateBorder = 1440
+              break
+            default:
+          }
+
+          const order = {
+            seller,
+            item: itemId,
+            timeLeft: auctionData[0],
+            timeLower: timestamp.clone().add(lowerDateBorder, 'm').toDate(),
+            timeUpper: timestamp.clone().add(upperDateBorder, 'm').toDate(),
+            estimatedTimestamp: timestamp.clone().add((lowerDateBorder + upperDateBorder) / 2, 'm').toDate(),
+            itemCount: auctionData[1],
+            minBid: auctionData[2],
+            buyout: auctionData[3],
+            currentBid: auctionData[4]
+          }
+          orders.push(order)
+        }
+      }
+    }
+
+    console.log(orders[1])
+    return orders
+  }
+}
+
+module.exports = Scan
