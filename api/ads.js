999d9d646b3f150f646e65a3c8b0be3fbbbffc51
Kaptard <kilian@nexus-stats.com>
2019-10-16 09:26:35 +0200

    feat: Implement ads resource for new domain

---
+++ api/ads.js
@@ -0,0 +1,14 @@
+const Endpoint = require('cubic-api/endpoint')
+
+class Ads extends Endpoint {
+  constructor (options) {
+    super(options)
+    this.schema.url = '/ads.txt'
+  }
+
+  async main (req, res) {
+    res.status(301).redirect('https://api.nitropay.com/v1/ads-227.txt')
+  }
+}
+
+module.exports = Ads
