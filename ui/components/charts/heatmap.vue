de2eae3f68886917c445ea2ce067104ec91b3217
Nakroma <tarackobarack@gmail.com>
2019-10-23 22:15:14 +0200

    added day axis

--- ui/components/charts/heatmap.vue
+++ ui/components/charts/heatmap.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="row">
+    <div class="axis-days">
+      <div v-for="day in days" :key="day" class="time">
+        <span class="label">
+          {{ day }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 
 
 <script>
-import * as d3 from 'd3'
-
 export default {
-  props: ['data']
+  props: ['data'],
+
+  computed: {
+    // computed because this logic may change
+    days () {
+      return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    }
+  }
 }
 </script>
 
@@ -19,5 +31,20 @@ export default {
 
 .row {
   flex-wrap: nowrap;
+}
+
+.label {
+  color: $color-font-body !important;
+}
+
+.axis-days {
+  text-align: right;
+
+  span {
+    display: inline-block;
+    font-size: 0.8em;
+    margin-left: 7px;
+    margin-top: 2.5px;
+  }
 }
 </style>
