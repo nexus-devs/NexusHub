8f484b8a3fe7802811b6eae4de5c9585934325b7
Nakroma <tarackobarack@gmail.com>
2019-11-07 16:27:46 +0100

    added index server and region endpoint

---
+++ ui/endpoints/wow-classic/indexServer.js
@@ -0,0 +1,11 @@
+const Endpoint = require('cubic-ui/endpoint')
+
+class Search extends Endpoint {
+  constructor (options) {
+    super(options)
+    this.schema.url = '/wow-classic/:region/:server'
+    this.schema.view = '/sites/wow-classic/index.vue'
+  }
+}
+
+module.exports = Search
