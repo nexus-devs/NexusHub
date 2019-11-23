bc7c23a195c919bfff51293caa5cb00fb179d791
Nakroma <tarackobarack@gmail.com>
2019-11-23 17:47:47 +0100

    added wow classic verify indices hook

---
+++ hooks/wow-classic.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb').MongoClient
+
+class Hook {
+  /**
+   * Ensure database indices are set up correctly
+   */
+  async verifyIndices () {
+    cubic.log.verbose('Core      | verifying wow-classic indices')
+    const config = cubic.config.api
+    const db = await mongodb.connect(config.mongoUrl, { useNewUrlParser: true })
+    const verify = async (db, col, index) => {
+      return db.db(config.overrideEndpoint['/wow-classic'].mongoDb).collection(col).createIndex(index)
+    }
+
+    // Scan Data
+    await verify(db, 'scanData', {
+      scannedAt: -1,
+      item: 1,
+      slug: 1
+    })
+  }
+}
+
+module.exports = new Hook()
--- services/bootstrap/regular.js
+++ services/bootstrap/regular.js
@@ -3,6 +3,7 @@ module.exports = async function () {
   const Auth = require('cubic-auth')
   const Ui = require('cubic-ui')
   const wfhooks = require('../../hooks/warframe.js')
+  const wowhooks = require('../../hooks/wow-classic.js')
   const config = {
     auth: require('../../config/cubic/auth.js'),
     ui: require('../../config/cubic/ui.js'),
@@ -20,6 +21,7 @@ module.exports = async function () {
   // split into separate nodes for better scaling
   cubic.hook(Api, wfhooks.verifyIndices)
   cubic.hook(Api, wfhooks.verifyItemList.bind(wfhooks))
+  cubic.hook(Api, wowhooks.verifyIndices)
   await cubic.use(new Api(config.api))
 
   // Load services
