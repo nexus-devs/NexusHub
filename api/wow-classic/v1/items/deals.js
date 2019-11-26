9758696b4f4688734a7bdf1ed2cef486730d8499
Nakroma <tarackobarack@gmail.com>
2019-11-26 17:05:19 +0100

    implemented deals endpoint

--- api/wow-classic/v1/items/deals.js
+++ api/wow-classic/v1/items/deals.js
@@ -9,14 +9,58 @@ class Deals extends Endpoint {
     this.schema.description = 'Get the best possible deals.'
     this.schema.url = '/wow-classic/v1/items/:slug/deals'
     this.schema.request = { url: '/wow-classic/v1/items/anathema-alliance/deals' }
-    this.schema.response = {}
+    this.schema.query = [
+      {
+        name: 'limit',
+        default: 4,
+        description: 'Number of best possible deals to return.'
+      }
+    ]
+    this.schema.response = [{
+      itemId: Number,
+      marketValue: Number,
+      minBuyout: Number,
+      dealDiff: Number
+    }]
   }
 
   /**
    * Main method which is called by EndpointHandler on request
    */
   async main (req, res) {
-    return res.send('no deal')
+    const slug = req.params.slug
+    const limit = req.query.limit
+
+    const lastScan = await this.db.collection('scans').findOne({ slug }, { sort: { scannedAt: -1 } })
+    if (!lastScan) {
+      return res.status(404).send({
+        error: 'Not found.',
+        reason: `Scans for ${slug} could not be found. Either there are no scans for that realm, or that realm doesn't exist.`
+      })
+    }
+
+    const data = await this.db.collection('scanData').aggregate([
+      { $match: { slug, scannedAt: lastScan.scannedAt } }, {
+        $project: {
+          _id: 0,
+          itemId: '$item',
+          marketValue: '$market_value',
+          minBuyout: '$min_buyout',
+          dealDiff: { $subtract: ['$market_value', '$min_buyout'] }
+        }
+      },
+      { $sort: { dealDiff: -1 } },
+      { $limit: limit }
+    ]).toArray()
+
+    if (!data.length) {
+      return res.status(404).send({
+        error: 'Not found.',
+        reason: `Deals for ${slug} could not be found. Either there are no recent entries for that realm, or that realm doesn't exist.`
+      })
+    }
+
+    return res.send(data)
   }
 }
 
