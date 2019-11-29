5e160b1bb705198bfdf830c2e09ce37f55a486de
Nakroma <tarackobarack@gmail.com>
2019-11-29 18:10:29 +0100

    added meta and pricing info to crafting api endpoint

--- api/wow-classic/v1/crafting/crafting.js
+++ api/wow-classic/v1/crafting/crafting.js
@@ -31,24 +31,72 @@ class Crafting extends Endpoint {
       ]
     }).toArray()
 
-    const createdBy = []
+    let createdBy = []
     const reagentFor = []
+    const itemStorage = {}
+
+    // Add raw data to return arrays, also to storage for later data processing
     for (const entry of items) {
       if (entry.itemId === item) {
-        createdBy.concat(entry.createdBy)
+        createdBy = createdBy.concat(entry.createdBy)
       } else {
         entry.createdBy = entry.createdBy.filter((x) => x.reagents.some((e) => e.itemId === item))
         const reagent = {
           itemId: entry.itemId,
           name: entry.name,
-          icon: `https://wow.zamimg.com/images/wow/icons/large/${entry.icon}.jpg`,
-          createdBy: entry.createdBy
+          icon: `https://wow.zamimg.com/images/wow/icons/large/${entry.icon}.jpg`
         }
-        reagentFor.push(reagent)
+        if (!itemStorage[entry.itemId] || !Object.keys(itemStorage[entry.itemId]).length) itemStorage[entry.itemId] = reagent
+        reagentFor.push({ ...reagent, createdBy: entry.createdBy })
+      }
+
+      for (const e of entry.createdBy) {
+        for (const x of e.reagents) if (!itemStorage[x.itemId]) itemStorage[x.itemId] = {}
       }
     }
 
-    return res.send({ itemId: item, slug, createdBy, reagentFor })
+    // Get missing item meta and price data
+    const parallel = []
+    parallel.push(this.db.collection('items').find({
+      itemId: { $in: Object.keys(itemStorage).filter((key) => !Object.keys(itemStorage[key]).length).map((v) => parseInt(v)) }
+    }).toArray())
+    for (const i of Object.keys(itemStorage).map((v) => parseInt(v))) {
+      parallel.push(this.db.collection('scanData').findOne({ item: i, slug }, { sort: { scannedAt: -1 } }))
+    }
+    const [metaData, ...priceData] = await Promise.all(parallel)
+
+    // Merge missing data
+    for (const i of Object.keys(itemStorage).map((v) => parseInt(v))) {
+      const entryMeta = metaData.find((x) => x.itemId === i)
+      const entryPrice = priceData.find((x) => x ? x.item === i : false)
+      itemStorage[i] = { ...itemStorage[i], ...entryMeta, ...entryPrice }
+    }
+
+    // Apply missing data
+    const applyData = (cby) => {
+      const reagents = cby.reagents.map((r) => {
+        const storage = itemStorage[r.itemId]
+        return {
+          ...r,
+          name: storage.name,
+          icon: storage.icon,
+          marketValue: storage.market_value ? storage.market_value : null
+        }
+      })
+      return { ...cby, reagents }
+    }
+    const applyReagentFor = (r) => {
+      if (itemStorage[r.itemId].market_value) r.marketValue = itemStorage[r.itemId].market_value
+      const createdBy = r.createdBy.map(applyData)
+      return { ...r, createdBy }
+    }
+
+    return res.send({
+      itemId: item,
+      slug,
+      createdBy: createdBy.map(applyData),
+      reagentFor: reagentFor.map(applyReagentFor)
+    })
   }
 }
 
