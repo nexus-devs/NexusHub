65e7d82317f173fa7d3cc58aedd336e41dac423c
Nakroma <tarackobarack@gmail.com>
2019-12-17 13:33:06 +0100

    implemented tsm wrapper for scans service

--- services/wow-classic/scans.js
+++ services/wow-classic/scans.js
@@ -2,9 +2,8 @@
  * Checks if new scans from TSM exist and add them to the database.
  * Only do this every 10 minutes, since new scans are only processed at max 1 per hour.
  */
-const request = require('request-promise')
-const fs = require('fs')
 const getClient = require('../getClient.js')
+const TSMRequest = require(`${process.cwd()}/api/lib/tsm-request.js`)
 const prod = process.env.NODE_ENV === 'production'
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 if (prod) {
@@ -14,8 +13,8 @@ if (prod) {
 
 async function monitor () {
   const client = await getClient()
-  const tsmKey = process.env.TSM_API_KEY || fs.readFileSync('/run/secrets/tsm-api-key', 'utf-8').trim()
   let lastDone = new Date()
+  const TSMReq = new TSMRequest()
 
   // Kill service if it gets stuck. Docker will auto-restart it.
   setInterval(() => {
@@ -27,7 +26,7 @@ async function monitor () {
   while (true) {
     const timer = new Date()
 
-    const reqRealms = await requestTSM(tsmKey, '/realms')
+    const reqRealms = await TSMReq.get('/realms')
     if (!reqRealms.success) console.log(`Could not fetch realms: ${reqRealms.error}`)
     else {
       const realms = reqRealms.data
@@ -38,7 +37,7 @@ async function monitor () {
         lastScan.scannedAt = lastScan.error ? new Date(0) : new Date(lastScan.scannedAt)
         const lastScanUnix = Math.floor(lastScan.scannedAt.getTime() / 1000)
         if (lastScanUnix < realm.last_modified) {
-          const scans = await requestTSM(tsmKey, `/realm/${realm.master_slug}/scans`)
+          const scans = await TSMReq.get(`/realm/${realm.master_slug}/scans`)
           if (!scans.success) {
             console.log(`Could not fetch scans for ${realm.master_slug}: ${reqRealms.error}`)
             break
@@ -47,7 +46,7 @@ async function monitor () {
           // Sort TSM scans by date and add them
           // Do old -> new so there aren't data holes if the service get's interrupted
           scans.data.sort((a, b) => a.last_modified - b.last_modified)
-          console.log(`Inserting ${scans.data.length} scans for ${realm.master_slug}`)
+          console.log(`Inserting ${scans.data.length} scans for ${realm.master_slug}...`)
           for (const scan of scans.data) {
             if (scan.last_modified <= lastScanUnix) continue // If scan is already added
 
@@ -56,6 +55,7 @@ async function monitor () {
             // Await to avoid overloading the TSM servers
             await client.post('/wow-classic/v1/scans/new', { slug: realm.master_slug, region: realm.region, scanId: scan.id, scannedAt })
           }
+          console.log('   ...done')
         }
       }
     }
@@ -64,14 +64,6 @@ async function monitor () {
     await sleep(1000 * 60 * 10)
     lastDone = new Date()
   }
-}
-
-async function requestTSM (tsmKey, endpoint) {
-  return request({
-    uri: `http://api2.tradeskillmaster.com${endpoint}`,
-    json: true,
-    headers: { 'User-Agent': 'Request-Promise', 'X-API-Key': tsmKey }
-  })
 }
 
 monitor()
