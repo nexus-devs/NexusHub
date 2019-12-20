a07bbfea380da8c168a2fada5fb61912bf8f4acc
Nakroma <tarackobarack@gmail.com>
2019-12-20 16:20:39 +0100

    Added server list verification test

--- ci/test/smoke/endpoints.js
+++ ci/test/smoke/endpoints.js
@@ -37,6 +37,7 @@ before(async function () {
     it('should pass WoW Classic hooks', async function () {
       await wowhooks.verifyIndices()
       await wowhooks.verifyItemList()
+      await wowhooks.verifyServerList()
     })
 
     it('should prime database with test order', async function () {
