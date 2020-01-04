393affc7a3acd984cfc0606f50d7fc14f44beefa
Nakroma <tarackobarack@gmail.com>
2020-01-04 15:04:56 +0100

    Fixed hooks not binding on api node correctly

--- services/bootstrap/docker.js
+++ services/bootstrap/docker.js
@@ -22,8 +22,8 @@ module.exports = async function () {
     cubic.hook(Api, wfhooks.verifyIndices)
     cubic.hook(Api, wfhooks.verifyItemList.bind(wfhooks))
     cubic.hook(Api, wowhooks.verifyIndices)
-    cubic.hook(Api, wowhooks.verifyItemList())
-    cubic.hook(Api, wowhooks.verifyServerList())
+    cubic.hook(Api, wowhooks.verifyItemList.bind(wowhooks))
+    cubic.hook(Api, wowhooks.verifyServerList.bind(wowhooks))
     await cubic.use(new Api(config.api))
   }
 
