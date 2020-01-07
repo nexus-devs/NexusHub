e402694585c178b908ae7d7f2b2b84693771e8ef
Nakroma <tarackobarack@gmail.com>
2020-01-07 13:56:46 +0100

    Added wow classic sitemap generation

--- ci/sitemap.js
+++ ci/sitemap.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const mongodb = require('mongodb').MongoClient
 const config = require(`${process.cwd()}/config/cubic/ui.js`)
 const wfhooks = require(`${process.cwd()}/hooks/warframe.js`)
+const wowhooks = require(`${process.cwd()}/hooks/wow-classic.js`)
 if (process.env.DRONE) {
   config.api.mongoUrl = 'mongodb://mongodb'
   config.api.redisUrl = 'redis://redis'
@@ -38,7 +39,11 @@ async function generate () {
   await cubic.use(new Api(apiConfig))
   await cubic.use(new Ui(config))
   await wfhooks.verifyItemList()
-  console.log('* Verified item list!')
+  console.log('* Verified warframe item list!')
+  await wowhooks.verifyItemList()
+  console.log('* Verified wow-classic item list!')
+  await wowhooks.verifyServerList()
+  console.log('* Verified wow-classic server list!')
 
   // Generate static page sitemap
   for (const endpoint of cubic.nodes.ui.api.server.http.endpoints.endpoints) {
@@ -56,6 +61,19 @@ async function generate () {
     if (item.tradable) sitemap.push(`https://nexushub.co${item.webUrl}/prices`)
     if (item.tradable) sitemap.push(`https://nexushub.co${item.webUrl}/trading`)
     if (item.patchlogs) sitemap.push(`https://nexushub.co${item.webUrl}/patchlogs`)
+  }
+
+  // Generate wow-classic item and server pages
+  const mongoWow = (await mongodb.connect(cubic.config.api.mongoUrl, { useNewUrlParser: true }))
+    .db(cubic.config.api.overrideEndpoint['/wow-classic'].mongoDb)
+  const itemsWow = await mongoWow.collection('items').find().toArray()
+  const servers = await mongoWow.collection('server').find().toArray()
+  for (const server of servers) {
+    sitemap.push(`https://nexushub.co/wow-classic/${server.slug}`)
+    for (const item of itemsWow) {
+      sitemap.push(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}`)
+      sitemap.push(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}/crafting`)
+    }
   }
 
   // Save to file
