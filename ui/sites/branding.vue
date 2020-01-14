19d64d1e57108205d0266a8f19eb06e68b5ec52a
Nakroma <tarackobarack@gmail.com>
2020-01-14 16:58:40 +0100

    Added PNGs to branding

--- ui/sites/branding.vue
+++ ui/sites/branding.vue
@@ -29,7 +29,7 @@
               <img src="/img/brand/nexushub-logo-typeface-wow-classic.svg" alt="Full Color NexusHub WoW Classic Logo">
               <div class="logo-download">
                 <a href="/img/brand/nexushub-logo-typeface-wow-classic.svg" download>.svg</a>
-                <a href="/img/brand/nexushub-logo-typeface-color.png" download>.png</a>
+                <a href="/img/brand/nexushub-logo-typeface-wow-classic.png" download>.png</a>
               </div>
             </div>
           </div>
@@ -52,14 +52,14 @@
               <img src="/img/brand/nexushub-logo-color-wow-classic.svg" alt="Full Color NexusHub WoW Classic Logo">
               <div class="logo-download">
                 <a href="/img/brand/nexushub-logo-color-wow-classic.svg" download>.svg</a>
-                <a href="/img/brand/nexushub-logo-color.png" download>.png</a>
+                <a href="/img/brand/nexushub-logo-color-wow-classic.png" download>.png</a>
               </div>
             </div>
             <div class="col logo-alt">
               <img src="/img/brand/nexushub-logo-typeface-v-color-wow-classic.svg" alt="Full Color NexusHub WoW Classic Logo">
               <div class="logo-download">
                 <a href="/img/brand/nexushub-logo-typeface-v-color-wow-classic.svg" download>.svg</a>
-                <a href="/img/brand/nexushub-logo-typeface-v-color.png" download>.png</a>
+                <a href="/img/brand/nexushub-logo-typeface-v-color-wow-classic.png" download>.png</a>
               </div>
             </div>
           </div>
