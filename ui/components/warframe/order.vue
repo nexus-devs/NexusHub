db661c623dc48adef7b628b49af98144b5f9cec4
Kaptard <kilian@nexus-stats.com>
2020-02-14 11:21:39 +0100

    fix(ui): Use proper ingame shortcuts in copy paste message

--- ui/components/warframe/order.vue
+++ ui/components/warframe/order.vue
@@ -67,7 +67,7 @@
             <span ref="message">
               <span>/w {{ order.user }}</span> Hi {{ user }},
               I'd like to {{ order.offer === 'Selling' ? 'buy' : 'sell' }}
-              [{{ item.name }} {{ order.component }}]{{ order.price ? ' for ' : '' }}{{ order.price ? `${order.price}p` : '' }}.
+              [{{ order.component === 'Set' ? item.name : `${item.name} ${order.component}` }}]{{ order.price ? ' for ' : '' }}{{ order.price ? `${order.price}p` : '' }}.
               {{ order.source === 'Warframe Market' ? '' : 'Found your offer on NexusHub.' }}
             </span>
             <div class="copy">
