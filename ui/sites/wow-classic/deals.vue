f743e7141f929133c18b62b5985e96cbdb4a971b
Nakroma <tarackobarack@gmail.com>
2020-02-19 14:48:07 +0100

    Added really basic deal list

--- ui/sites/wow-classic/deals.vue
+++ ui/sites/wow-classic/deals.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="search">
+  <div class="deals">
     <navigation />
     <app-content>
       <section class="results">
-        <div class="results-background-fix" />
         <div class="container">
-          <div class="results-headline">
-            <span>Results for</span>
-            <!--<h1>{{ input }}</h1>-->
+          <div class="deals-headline">
+            <h1>Deals</h1>
           </div>
-          <!-- <results-group :type="'items'" /> -->
+          <div class="deal-group">
+            <div class="filter">
+
+            </div>
+            <div class="deal-container">
+              <transition-group class="deal-list">
+                <div v-for="deal in deals" :key="deal.itemId" class="deal">
+                  <router-link :to="`/wow-classic/items/${server}/${deal.itemId}`" class="row interactive">
+                    <img :src="deal.icon" class="deal-img-blur" :alt="deal.name">
+                    <div class="deal-title col-b">
+                      <div class="deal-img">
+                        <img :src="deal.icon" :class="{ mod: deal.icon.includes('jpeg') }" :alt="deal.name">
+                      </div>
+                      <span>{{ deal.name }}</span>
+                    </div>
+                  </router-link>
+                </div>
+              </transition-group>
+            </div>
+          </div>
         </div>
         <ad name="wow-classic-profitable-deals" />
       </section>
@@ -24,19 +41,43 @@ import ad from 'src/components/ads/nitroAds.vue'
 import appContent from 'src/app-content.vue'
 import meta from 'src/components/seo/meta.js'
 import navigation from 'src/components/ui/nav/wow-classic.vue'
-import resultsGroup from 'src/components/search/results-wow-classic/results-group.vue'
+import storeModule from 'src/store/wow-classic/wowclassic.js'
 
 export default {
   components: {
     ad,
     navigation,
-    appContent,
-    resultsGroup
+    appContent
   },
 
   async asyncData ({ store, route }) {
     const slug = route.params.slug
+    const deals = await this.$cubic.get(`/wow-classic/v1/items/${slug}/deals`)
+
+    const parallel = []
+    for (const deal of deals) parallel.push(this.$cubic.get(`/wow-classic/v1/items/${slug}/${deal.itemId}`))
+    const itemData = await Promise.all(parallel)
+
+    for (const deal of deals) {
+      const item = itemData.shift()
+      deal.icon = item.icon
+      deal.name = item.name
+      deal.percentage = ((deal.marketValue - deal.minBuyout) / deal.minBuyout * 100).toFixed(2)
+    }
+
+    store.commit('setDeals', deals)
   },
+
+  computed: {
+    deals () {
+      return this.$store.state.wowclassic.deals
+    },
+    server () {
+      return this.$store.state.servers.server
+    }
+  },
+
+  storeModule
 
   /* head () {
     return {
@@ -56,138 +97,118 @@ export default {
 <style lang="scss" scoped>
 @import '~src/styles/partials/wow-classic/importer';
 
-.search-input {
-  position: relative;
-  z-index: 1;
-  border-bottom: 1px solid $color-subtle-dark;
+.deal-container {
+  position: relative; // for position: absolute item list views
+  overflow: hidden;
+  @include ease-out(0.35s); // When results block gets resized
+}
+.deal {
+  @include ease(0.5s);
 
-  @media (max-width: $breakpoint-m) {
-    border-top: none;
+  .row {
+    justify-content: space-between;
   }
-
-  .container {
+  .deal-title {
+    position: relative;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-
-    @media (max-width: $breakpoint-s) {
-      // wrap category
-      display: block;
-      margin-top: 30px;
-    }
   }
-
-  .field {
+  .deal-img-blur {
+    position: absolute;
+    left:0;
+    height: 400%;
+    opacity: 0.4;
+    z-index: 0;
+    filter: blur(80px);
+  }
+  .deal-img {
     position: relative;
-    margin: 24px 0 20px;
-    padding: 10px;
-    max-width: 250px;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    height: 38px;
+    width: 38px;
     border-radius: 2px;
-    background: $color-bg-darker;
-    @include ease(0.35s);
+    margin-right: 20px;
 
-    @media (max-width: $breakpoint-m) {
-      margin: 20px 0 14px;
-    }
-    @media (max-width: $breakpoint-s) {
-      margin: 24px 0;
-      max-width: 100%;
-    }
-    input {
-      position: relative;
-      z-index: 1;
+    img {
       width: 100%;
-      color: white;
-      margin-left: 25px;
+    }
+    img.mod {
+      border-radius: 4px;
     }
   }
-
-  .search-ico {
-    position: absolute;
-    top: 11px;
+  .deal-data {
+    position: relative;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 75%;
   }
-
-  .search-filters {
-    .search-filters-label {
-      display: flex;
-      align-items: center;
-      @include uppercase;
-      font-size: 0.85em;
-      color: white;
-
-      @media (max-width: $breakpoint-s) {
-        position: absolute;
-        right: 50px;
-        top: 0;
-        z-index: 1;
-      }
-      @media (max-width: $breakpoint-xs) {
-        position: absolute;
-        right: 30px;
-        top: 0;
-        z-index: 1;
-      }
-    }
-  }
-}
-
-.search-filters-options {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 20px;
-  height: 142px;
-  background: $color-bg-darker;
-
-  .container {
-    opacity: 0;
-    @include ease(0.35s);
-    transition-delay: 0.1s;
-  }
-  &.expanded {
-    .container {
-      opacity: 1;
-    }
-  }
-}
-
-.results {
-  position: relative;
-  padding-top: 30px;
-  padding-bottom: 0;
-  margin-top: 0;
-  margin-bottom: 0;
-  transform: translateY(-142px);
-  will-change: transform margin-bottom;
-  @include ease-out(0.35s);
-
-  &.expanded {
-    transform: translateY(0);
-    margin-bottom: 142px;
-  }
-  .container {
-    position: relative; // fix for results-background-fix covering contents
-  }
-  .results-headline {
-    padding: 30px 0;
-  }
-  // Cover filters with just as much background as necessary. Otherwise chrome
-  // has some color banding issues with its gpu acceleration due to lots of
-  // transform elements above the background.
-  .results-background-fix {
-    position: absolute;
-    top: 0;
-    height: 142px;
-    width: 100%;
-    background: $color-bg-dark;
-  }
-}
-
-/deep/ .result-info {
-  text-align: center;
-
-  span {
+  .deal-data-value {
     font-size: 0.9em;
+    white-space: nowrap;
+
+    &:last-of-type {
+      flex-grow: 0;
+      text-align: right;
+      margin-right: 0;
+    }
+  }
+  @media (max-width: $breakpoint-s) {
+    .deal-title {
+      span {
+        position: relative;
+        top: -10px;
+      }
+    }
+    .deal-img {
+      margin-right: 10px;
+      height: 50px;
+      width: 50px;
+    }
+    .deal-img-blur {
+      left: -50%;
+    }
+    .deal-data {
+      justify-content: flex-start;
+      top: -23px;
+      margin-bottom: -20px;
+      margin-left: 60px;
+    }
+    .deal-data-value {
+      font-size: 0.85em;
+      margin-right: 10px;
+
+      img {
+        height: 10px;
+      }
+      &.hidden {
+        display: none;
+      }
+    }
+  }
+}
+
+a {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  @include field;
+  align-items: center;
+  border-radius: 2px;
+  padding: 10px 20px;
+  margin-bottom: 8px;
+  @include ease(0.05s);
+
+  &:hover {
+    opacity: 1 !important;
+    transform: scale(0.995);
+  }
+  &:active {
+    transform: scale(0.995);
+  }
+  &:before {
+    border-radius: 0;
   }
 }
 </style>
