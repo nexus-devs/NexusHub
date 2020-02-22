f04ecf017bc8898c8c4fc0c2b99d6a0a2abecefd
Nakroma <tarackobarack@gmail.com>
2020-02-22 01:04:17 +0100

    Sitemap now gets split into several chunks

--- ci/sitemap.js
+++ ci/sitemap.js
@@ -15,88 +15,123 @@ if (process.env.DRONE) {
   config.webpack.skipBuild = true
 }
 
-async function generate () {
-  const sitemap = []
-  let sitemapCounter = 0
-  const Cubic = require('cubic')
+class Sitemap {
+  constructor () {
+    this.sitemap = []
+    this.sitemapCounter = 0
+  }
 
-  // eslint-disable-next-line no-new
-  new Cubic({ logLevel: 'silent', skipAuthCheck: true })
+  /**
+   * Start sitemap generation
+   */
+  async generate () {
+    const Cubic = require('cubic')
 
-  // Load up UI node
-  const Api = require('cubic-api')
-  const Auth = require('cubic-auth')
-  const Ui = require('cubic-ui')
-  const apiConfig = require('../config/cubic/api.js')
-  if (process.env.DRONE) apiConfig.mongoUrl = 'mongodb://mongodb'
-  if (process.env.DRONE) apiConfig.redisUrl = 'redis://redis'
-  const authConfig = process.env.DRONE ? {
-    api: {
-      mongoUrl: 'mongodb://mongodb',
-      redisUrl: 'redis://redis'
+    // eslint-disable-next-line no-new
+    new Cubic({ logLevel: 'silent', skipAuthCheck: true })
+
+    // Load up UI node
+    const Api = require('cubic-api')
+    const Auth = require('cubic-auth')
+    const Ui = require('cubic-ui')
+    const apiConfig = require('../config/cubic/api.js')
+    if (process.env.DRONE) apiConfig.mongoUrl = 'mongodb://mongodb'
+    if (process.env.DRONE) apiConfig.redisUrl = 'redis://redis'
+    const authConfig = process.env.DRONE ? {
+      api: {
+        mongoUrl: 'mongodb://mongodb',
+        redisUrl: 'redis://redis'
+      }
+    } : {}
+    await cubic.use(new Auth(authConfig))
+    await cubic.use(new Api(apiConfig))
+    await cubic.use(new Ui(config))
+    await wfhooks.verifyItemList()
+    console.log('* Verified warframe item list!')
+    await wowhooks.verifyItemList()
+    console.log('* Verified wow-classic item list!')
+    await wowhooks.verifyServerList()
+    console.log('* Verified wow-classic server list!')
+
+    await this.generateStaticPages()
+    await this.generateWarframePages()
+    await this.generateWowClassicPages()
+
+    if (this.sitemapCounter > 0) {
+      fs.writeFileSync(`${cubic.config.ui.api.publicPath}/sitemap${this.sitemapCounter}.txt`, this.sitemap.join('\n'))
     }
-  } : {}
-  await cubic.use(new Auth(authConfig))
-  await cubic.use(new Api(apiConfig))
-  await cubic.use(new Ui(config))
-  await wfhooks.verifyItemList()
-  console.log('* Verified warframe item list!')
-  await wowhooks.verifyItemList()
-  console.log('* Verified wow-classic item list!')
-  await wowhooks.verifyServerList()
-  console.log('* Verified wow-classic server list!')
+    this.generateSitemapIndex()
+    console.log(`* Saved ${this.sitemapCounter} sitemaps with ${this.sitemapCounter * 50000 + this.sitemap.length} entries.`)
+  }
 
-  // Create write stream. This is because the sitemap array runs out of memory eventually.
-  // Deletion needed because write stream doesn't overwrite anymore
-  if (fs.existsSync(`${cubic.config.ui.api.publicPath}/sitemap.txt`)) fs.unlinkSync(`${cubic.config.ui.api.publicPath}/sitemap.txt`)
-  const stream = fs.createWriteStream(`${cubic.config.ui.api.publicPath}/sitemap.txt`, { flags: 'a' })
-
-  // Generate static page sitemap
-  for (const endpoint of cubic.nodes.ui.api.server.http.endpoints.endpoints) {
-    if (!endpoint.route.includes('/:')) {
-      sitemap.push(`https://nexushub.co${endpoint.route}`)
+  /**
+   * Generate static pages
+   */
+  async generateStaticPages () {
+    for (const endpoint of cubic.nodes.ui.api.server.http.endpoints.endpoints) {
+      if (!endpoint.route.includes('/:')) {
+        this.pushToSitemap(`https://nexushub.co${endpoint.route}`)
+      }
     }
   }
 
-  // Generate warframe item pages
-  const mongo = (await mongodb.connect(cubic.config.api.mongoUrl, { useNewUrlParser: true }))
-    .db(cubic.config.api.mongoDb)
-  const items = await mongo.collection('items').find().toArray()
-  for (const item of items) {
-    sitemap.push(`https://nexushub.co${item.webUrl}`)
-    if (item.tradable) sitemap.push(`https://nexushub.co${item.webUrl}/prices`)
-    if (item.tradable) sitemap.push(`https://nexushub.co${item.webUrl}/trading`)
-    if (item.patchlogs) sitemap.push(`https://nexushub.co${item.webUrl}/patchlogs`)
-  }
-
-  sitemapCounter += writeSitemapToStream(stream, sitemap)
-
-  // Generate wow-classic item and server pages
-  const mongoWow = (await mongodb.connect(cubic.config.api.mongoUrl, { useNewUrlParser: true }))
-    .db(cubic.config.api.overrideEndpoint['/wow-classic'].mongoDb)
-  const itemsWow = await mongoWow.collection('items').find().toArray()
-  const servers = await mongoWow.collection('server').find().toArray()
-  for (const server of servers) {
-    sitemap.push(`https://nexushub.co/wow-classic/${server.slug}`)
-    for (const item of itemsWow) {
-      sitemap.push(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}`)
-      sitemap.push(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}/crafting`)
+  /**
+   * Generate warframe pages
+   */
+  async generateWarframePages () {
+    const mongo = (await mongodb.connect(cubic.config.api.mongoUrl, { useNewUrlParser: true }))
+      .db(cubic.config.api.mongoDb)
+    const items = await mongo.collection('items').find().toArray()
+    for (const item of items) {
+      this.pushToSitemap(`https://nexushub.co${item.webUrl}`)
+      if (item.tradable) this.pushToSitemap(`https://nexushub.co${item.webUrl}/prices`)
+      if (item.tradable) this.pushToSitemap(`https://nexushub.co${item.webUrl}/trading`)
+      if (item.patchlogs) this.pushToSitemap(`https://nexushub.co${item.webUrl}/patchlogs`)
     }
-
-    sitemapCounter += writeSitemapToStream(stream, sitemap)
   }
 
-  // Save to file
-  console.log(`* Saved sitemap with ${sitemapCounter} entries.`)
-  stream.end()
+  /**
+   * Generate wow-classic pages
+   */
+  async generateWowClassicPages () {
+    const mongoWow = (await mongodb.connect(cubic.config.api.mongoUrl, { useNewUrlParser: true }))
+      .db(cubic.config.api.overrideEndpoint['/wow-classic'].mongoDb)
+    const itemsWow = await mongoWow.collection('items').find().toArray()
+    const servers = await mongoWow.collection('server').find().toArray()
+    for (const server of servers) {
+      this.pushToSitemap(`https://nexushub.co/wow-classic/${server.slug}`)
+      for (const item of itemsWow) {
+        this.pushToSitemap(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}`)
+        this.pushToSitemap(`https://nexushub.co/wow-classic/items/${server.slug}/${item.itemId}/crafting`)
+      }
+    }
+  }
+
+  /**
+   * Generates the main sitemap index file that points to each sitemap
+   * For this trivial xml there is no need for a third party lib
+   */
+  generateSitemapIndex () {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    for (let i = 0; i <= this.sitemapCounter; i++) {
+      xml += `\t<sitemap><loc>https://nexushub.co/sitemap${i}.txt</loc></sitemap>\n`
+    }
+    xml += '</sitemapindex>'
+    fs.writeFileSync(`${cubic.config.ui.api.publicPath}/sitemap.xml`, xml)
+  }
+
+  /**
+   * Helper function to chunk sitemap automatically
+   */
+  pushToSitemap (value) {
+    if (this.sitemap.length >= 50000) { // Max 50k entries per sitemap file
+      fs.writeFileSync(`${cubic.config.ui.api.publicPath}/sitemap${this.sitemapCounter}.txt`, this.sitemap.join('\n'))
+      this.sitemapCounter++
+      this.sitemap = []
+    }
+    this.sitemap.push(value)
+  }
 }
 
-// Helper function to write sitemap to stream, empty it and return the count
-function writeSitemapToStream (stream, sitemap) {
-  const count = sitemap.length
-  stream.write(`${sitemap.join('\n')}\n`)
-  sitemap.splice(0, count) // Empty array reference
-  return count
-}
-
-generate().then(() => process.exit())
+const sitemap = new Sitemap()
+sitemap.generate().then(() => process.exit())
