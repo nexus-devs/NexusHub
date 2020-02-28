6597baaed4b7c415964990808905a30cfe98c758
Nakroma <tarackobarack@gmail.com>
2020-02-28 17:27:03 +0100

    feat: Added item overview endpoint (resolves #342)

---
+++ api/wow-classic/v1/items/overview.js
@@ -0,0 +1,58 @@
+const Endpoint = require('cubic-api/endpoint')
+
+/**
+ * Provides basic item statistics for all items on a specific server
+ */
+class Overview extends Endpoint {
+  constructor (options) {
+    super(options)
+    this.schema.description = 'Get basic stats for all items on a server. Usage of this data for commercial purposes must be discussed with us before.'
+    this.schema.url = '/wow-classic/v1/items/:server'
+    this.schema.request = { url: '/wow-classic/v1/items/anathema-alliance' }
+    const priceObj = {
+      marketValue: Number,
+      historicalValue: Number,
+      minBuyout: Number,
+      numAuctions: Number,
+      quantity: Number
+    }
+    this.schema.response = {
+      slug: String,
+      data: [{
+        itemId: Number,
+        previous: priceObj,
+        ...priceObj
+      }]
+    }
+  }
+
+  /**
+   * Main method which is called by EndpointHandler on request
+   */
+  async main (req, res) {
+    const slug = req.params.server.toLowerCase()
+
+    const server = await this.db.collection('server').findOne({ slug })
+    if (!server) {
+      const response = {
+        error: 'Not found.',
+        reason: `Server ${slug} could not be found.`
+      }
+      this.cache(response, 60 * 60)
+      return res.status(404).send(response)
+    }
+
+    const data = await this.db.collection('currentData').find({ slug }).toArray()
+    for (const entry of data) {
+      delete entry._id
+      delete entry.slug
+    }
+
+    const response = { slug, data }
+
+    // this.cache(response, 60)
+    return res.send(response)
+  }
+}
+
+module.exports = Overview
