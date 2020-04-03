997f88f489a74a3ce68e9e07f55761e7f0442a7e
Nakroma <tarackobarack@gmail.com>
2020-04-03 20:57:08 +0200

    Migrated /ga/views to reporting API v4

--- api/analytics/v1/ga/views.js
+++ api/analytics/v1/ga/views.js
@@ -1,6 +1,6 @@
 const Endpoint = require('cubic-api/endpoint')
 const { google } = require('googleapis')
-const analytics = google.analytics('v3')
+const analytics = google.analyticsreporting({ version: 'v4' })
 const options = require(`${process.cwd()}/config/ga/nexushub-ga-user.js`)
 
 class Index extends Endpoint {
@@ -18,19 +18,24 @@ class Index extends Endpoint {
     try {
       const auth = new google.auth.JWT(options.email, options.key, null, ['https://www.googleapis.com/auth/analytics.readonly'])
       await auth.authorize()
-      const ga = await analytics.data.ga.get({
+      const ga = await analytics.reports.batchGet({
         auth,
-        ids: options.ids,
-        metrics: 'ga:pageviews',
-        'start-date': '30daysAgo',
-        'end-date': 'today',
-        dimensions: 'ga:date'
+        requestBody: {
+          reportRequests: [{
+            viewId: options.viewId,
+            dateRanges: [{
+              startDate: '30daysAgo',
+              endDate: 'today'
+            }],
+            metrics: [{ expression: 'ga:pageviews' }]
+          }]
+        }
       })
-      const views = parseInt(ga.data.totalsForAllResults['ga:pageviews'])
+      const views = ga.data.reports[0].data.totals[0].values[0]
       res.send(views)
       this.cache(views, 60 * 60)
     } catch (err) {
-      res.send(1000000) // Placeholder if auth fails (which it would on dev builds)
+      res.send('666') // Placeholder if auth fails (which it would on dev builds)
     }
   }
 }
--- config/ga/nexushub-ga-user.js
+++ config/ga/nexushub-ga-user.js
@@ -4,5 +4,6 @@ module.exports = {
   email: 'company-page@nexushub-ga.iam.gserviceaccount.com',
   clientId: '116601860147515971555',
   key: docker ? '/run/secrets/nexus-ga-key' : `${__dirname}/nexushub-ga-private.pem`,
-  ids: 'ga:126211225'
+  ids: 'ga:126211225',
+  viewId: '184710426'
 }
