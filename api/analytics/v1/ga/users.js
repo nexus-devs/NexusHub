7ecc16d266b0fc71e1827439bccadb302e89c517
Nakroma <tarackobarack@gmail.com>
2020-04-03 21:00:36 +0200

    Migrated /ga/users to reporting API v4

--- api/analytics/v1/ga/users.js
+++ api/analytics/v1/ga/users.js
@@ -1,6 +1,6 @@
 const Endpoint = require('cubic-api/endpoint')
 const { google } = require('googleapis')
-const analytics = google.analytics('v3')
+const analytics = google.analyticsreporting({ version: 'v4' })
 const options = require(`${process.cwd()}/config/ga/nexushub-ga-user.js`)
 
 class Index extends Endpoint {
@@ -18,19 +18,24 @@ class Index extends Endpoint {
     try {
       const auth = new google.auth.JWT(options.email, options.key, null, ['https://www.googleapis.com/auth/analytics.readonly'])
       await auth.authorize()
-      const ga = await analytics.data.ga.get({
+      const ga = await analytics.reports.batchGet({
         auth,
-        ids: options.ids,
-        metrics: 'ga:30dayUsers',
-        'start-date': 'yesterday',
-        'end-date': 'today',
-        dimensions: 'ga:date'
+        requestBody: {
+          reportRequests: [{
+            viewId: options.viewId,
+            dateRanges: [{
+              startDate: '30daysAgo',
+              endDate: 'today'
+            }],
+            metrics: [{ expression: 'ga:users' }]
+          }]
+        }
       })
-      const users = parseInt(ga.data.totalsForAllResults['ga:30dayUsers'])
+      const users = ga.data.reports[0].data.totals[0].values[0]
       res.send(users)
       this.cache(users, 60 * 60)
     } catch (err) {
-      res.send(250000) // Placeholder if auth fails (which it would on dev builds)
+      res.send('999') // Placeholder if auth fails (which it would on dev builds)
     }
   }
 }
