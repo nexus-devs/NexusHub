b2ff204a39852fd09d04566e9f1f5dbbd6f993cb
Nakroma <tarackobarack@gmail.com>
2020-04-07 19:41:54 +0200

    Added release date to content phase 4

--- hooks/wow-classic.js
+++ hooks/wow-classic.js
@@ -112,7 +112,7 @@ class Hook {
       { contentPhase: 1, releaseDate: new Date(Date.UTC(2019, 7, 26)), description: 'Onyxia and Molten Core' },
       { contentPhase: 2, releaseDate: new Date(Date.UTC(2019, 10, 12)), description: 'Battlegrounds, Honor System and World Bosses' },
       { contentPhase: 3, releaseDate: new Date(Date.UTC(2020, 1, 12)), description: 'Blackwing Lair and Darkmoon Faire' },
-      { contentPhase: 4, releaseDate: null, description: 'Zul\'Gurub and Arathi Basin' },
+      { contentPhase: 4, releaseDate: new Date('04 15 2020 15:00:00 PDT'), description: 'Zul\'Gurub, Arathi Basin and the Green Dragons' },
       { contentPhase: 5, releaseDate: null, description: 'Ahn\'Qiraj' },
       { contentPhase: 6, releaseDate: null, description: 'Naxxramas' }
     ]
