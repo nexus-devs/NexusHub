8fcaceface7734f19d1f6f7aadd335eee624776b
nexus-ci <apps@nexus-stats.com>
2020-04-10 17:31:01 +0000

    ci: Add staging builds

---
+++ ui/assets/bundles/bundle.39.81ff2444dff5f4353933.js
@@ -0,0 +1,128 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[39],{
+
+/***/ 152:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _branding_vue_vue_type_template_id_3490c601_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(983);
+/* harmony import */ var _branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(472);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _branding_vue_vue_type_style_index_0_id_3490c601_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(887);
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(3);
+
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__[/* default */ "a"])(
+  _branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _branding_vue_vue_type_template_id_3490c601_scoped_true___WEBPACK_IMPORTED_MODULE_0__[/* render */ "a"],
+  _branding_vue_vue_type_template_id_3490c601_scoped_true___WEBPACK_IMPORTED_MODULE_0__[/* staticRenderFns */ "b"],
+  false,
+  null,
+  "3490c601",
+  null
+  
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 472:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(473);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ 473:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _appContent = __webpack_require__(177);
+
+var _appContent2 = _interopRequireDefault(_appContent);
+
+var _meta = __webpack_require__(174);
+
+var _meta2 = _interopRequireDefault(_meta);
+
+var _warframe = __webpack_require__(354);
+
+var _warframe2 = _interopRequireDefault(_warframe);
+
+var _header = __webpack_require__(660);
+
+var _header2 = _interopRequireDefault(_header);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+  components: {
+    appContent: _appContent2.default,
+    navigation: _warframe2.default,
+    uiHeader: _header2.default
+  },
+  head: {
+    title: 'NexusHub Brand',
+    meta: (0, _meta2.default)({
+      title: 'Brand Resources for NexusHub',
+      description: 'Branding resources for NexusHub. Get logos and guidelines to use our brand for whatever you\'re working on.'
+    })
+  }
+};
+
+/***/ }),
+
+/***/ 475:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 887:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_node_modules_css_loader_dist_cjs_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_style_index_0_id_3490c601_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(475);
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_node_modules_css_loader_dist_cjs_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_style_index_0_id_3490c601_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_node_modules_css_loader_dist_cjs_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_style_index_0_id_3490c601_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_node_modules_css_loader_dist_cjs_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_sass_loader_dist_cjs_js_node_modules_vue_loader_lib_index_js_vue_loader_options_branding_vue_vue_type_style_index_0_id_3490c601_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ 983:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./ui/sites/branding.vue?vue&type=template&id=3490c601&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('navigation'),_vm._v(" "),_c('app-content',[_c('ui-header',[_c('img',{attrs:{"src":"/img/brand/nexushub-typeface-bw.svg","alt":"NexusHub"}}),_vm._v(" "),_c('h1',[_vm._v("BRAND")]),_vm._v(" "),_c('p',[_vm._v("\n        NexusHub is an agile, reactive brand that is always in motion, like\n        the real-time data that we're serving to gamers worldwide.\n        Feel free to use the assets below when you need to reference our service.\n      ")])]),_vm._v(" "),_c('div',{staticClass:"container"},[_c('section',[_c('h2',[_vm._v("NexusHub Logo")]),_vm._v(" "),_c('p',[_vm._v("\n          Please do not edit, distort, recolor, or otherwise repurpose the NexusHub logo.\n        ")]),_vm._v(" "),_c('div',{staticClass:"row-margin"},[_c('div',{staticClass:"col logo"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-color.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-color.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-color.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-wow-classic.svg","alt":"Full Color NexusHub WoW Classic Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-wow-classic.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-wow-classic.png","download":""}},[_vm._v(".png")])])])]),_vm._v(" "),_c('div',{staticClass:"row row-margin"},[_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-color.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-color.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-color.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-v-color.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-color.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-color.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-color-wow-classic.svg","alt":"Full Color NexusHub WoW Classic Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-color-wow-classic.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-color-wow-classic.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-v-color-wow-classic.svg","alt":"Full Color NexusHub WoW Classic Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-color-wow-classic.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-color-wow-classic.png","download":""}},[_vm._v(".png")])])])]),_vm._v(" "),_c('div',{staticClass:"row-margin"},[_c('div',{staticClass:"col logo"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-bw.svg","alt":"Black/White NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-bw.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-bw.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-b.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-b.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-b.png","download":""}},[_vm._v(".png")])])])]),_vm._v(" "),_c('div',{staticClass:"row row-margin"},[_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-bw.svg","alt":"Black/White NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-bw.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-bw.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-v-bw.svg","alt":"Black/White NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-bw.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-bw.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-b.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-b.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-b.png","download":""}},[_vm._v(".png")])])]),_vm._v(" "),_c('div',{staticClass:"col logo-alt"},[_c('img',{attrs:{"src":"/img/brand/nexushub-logo-typeface-v-b.svg","alt":"Full Color NexusHub Logo"}}),_vm._v(" "),_c('div',{staticClass:"logo-download"},[_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-b.svg","download":""}},[_vm._v(".svg")]),_vm._v(" "),_c('a',{attrs:{"href":"/img/brand/nexushub-logo-typeface-v-b.png","download":""}},[_vm._v(".png")])])])])]),_vm._v(" "),_c('section',[_c('h2',[_vm._v("NexusHub Colors")]),_vm._v(" "),_c('p',[_vm._v("\n          Keep in mind that each color in our palette comes in two shades for\n          different situations.\n        ")]),_vm._v(" "),_c('div',{staticClass:"row-margin"},[_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade green"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#39e56f")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade green-shade"},[_c('span',[_vm._v("#68d891")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade blue"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#15ced6")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade blue-shade"},[_c('span',[_vm._v("#11acb2")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade grey"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#8b97a8")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade grey-shade"},[_c('span',[_vm._v("#2d323b")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade dark"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#21232a")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade dark-shade"},[_c('span',[_vm._v("#101115")])])])]),_vm._v(" "),_c('section',[_c('h2',[_vm._v("World of Warcraft Classic Colors")]),_vm._v(" "),_c('p',[_vm._v("\n            These are the colors our WoW Classic section uses.\n          ")]),_vm._v(" "),_c('div',{staticClass:"row-margin"},[_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade yellow"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#e6ad02")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade yellow-shade"},[_c('span',[_vm._v("#e0b534")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade blizzard-blue"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#00c0ff")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade blizzard-blue-shade"},[_c('span',[_vm._v("#23acd9")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade brown"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#a59787")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade brown-shade"},[_c('span',[_vm._v("#3b332d")])])]),_vm._v(" "),_c('div',{staticClass:"col-b color"},[_c('div',{staticClass:"primary-shade dark-brown"},[_c('span'),_vm._v(" "),_c('span',[_vm._v("#252120")])]),_vm._v(" "),_c('div',{staticClass:"sub-shade dark-brown-shade"},[_c('span',[_vm._v("#151310")])])])])])])])],1)],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./ui/sites/branding.vue?vue&type=template&id=3490c601&scoped=true&
+/* concated harmony reexport render */__webpack_require__.d(__webpack_exports__, "a", function() { return render; });
+/* concated harmony reexport staticRenderFns */__webpack_require__.d(__webpack_exports__, "b", function() { return staticRenderFns; });
+
+
+/***/ })
+
+}]);
