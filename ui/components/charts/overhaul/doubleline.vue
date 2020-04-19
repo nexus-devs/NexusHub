e338a704adbc5704641f3f22b02431524993b295
Nakroma <tarackobarack@gmail.com>
2020-04-19 22:03:30 +0200

    Added area to quantity

--- ui/components/charts/overhaul/doubleline.vue
+++ ui/components/charts/overhaul/doubleline.vue
@@ -23,7 +23,7 @@ export default {
       top: 0,
       bottom: 20,
       left: 50,
-      right: 5
+      right: 35
     }
     const boundingBox = svg.node().getBoundingClientRect()
     const height = boundingBox.height - padding.top - padding.bottom
@@ -38,37 +38,38 @@ export default {
     const yScale1 = d3.scaleLinear()
       .range([height, 0])
       .domain([yExtents1[0] - yPadding1 < 0 ? 0 : yExtents1[0] - yPadding1, yExtents1[1] + yPadding1])
-    const yExtents2 = d3.extent(data, d => d.y2)
-    const yPadding2 = Math.round((yExtents2[1] - yExtents2[0]) / 6)
     const yScale2 = d3.scaleLinear()
       .range([height, 0])
-      .domain([yExtents2[0] - yPadding2 < 0 ? 0 : yExtents2[0] - yPadding2, yExtents2[1] + yPadding2])
+      .domain([0, d3.max(data, d => d.y2) + Math.round(d3.max(data, d => d.y2) / 6)])
     const xScale = d3.scaleTime()
       .range([0, width])
       .domain(d3.extent(data, d => d.x))
 
-    // Create axes
-    chart.append('g') // Y1 left axis
-      .attr('class', 'axis')
-      .call(d3.axisLeft(yScale1).tickFormat(d => (d / 10000).toFixed(2) + 'g').tickSize(3).ticks(5).tickSizeOuter(0))
-    chart.append('g') // X axis
-      .attr('transform', `translate(0, ${height})`)
-      .attr('class', 'axis')
-      .call(d3.axisBottom(xScale).ticks(7).tickFormat(d3.timeFormat('%d. %b')).tickSize(4).tickSizeOuter(0))
-
     // Create lines
+    chart.append('path')
+      .datum(data)
+      .attr('class', 'line-2')
+      .attr('stroke-width', 1.5)
+      .attr('d', d3.area().x(d => xScale(d.x)).y0(yScale2(0)).y1(d => yScale2(d.y2)))
     chart.append('path')
       .datum(data)
       .attr('class', 'line-1')
       .attr('fill', 'none')
       .attr('stroke-width', 1.5)
       .attr('d', d3.line().x(d => xScale(d.x)).y(d => yScale1(d.y1)))
-    chart.append('path')
-      .datum(data)
-      .attr('class', 'line-2')
-      .attr('fill', 'none')
-      .attr('stroke-width', 1.5)
-      .attr('d', d3.line().x(d => xScale(d.x)).y(d => yScale2(d.y2)))
+
+    // Create axes
+    chart.append('g') // Y1 left axis
+      .attr('class', 'axis')
+      .call(d3.axisLeft(yScale1).tickFormat(d => (d / 10000).toFixed(2) + 'g').tickSize(3).ticks(5).tickSizeOuter(0))
+    chart.append('g') // Y2 left axis
+      .attr('transform', `translate(${width}, 0)`)
+      .attr('class', 'axis')
+      .call(d3.axisRight(yScale2).tickSize(3).ticks(5).tickSizeOuter(0))
+    chart.append('g') // X axis
+      .attr('transform', `translate(0, ${height})`)
+      .attr('class', 'axis')
+      .call(d3.axisBottom(xScale).ticks(7).tickFormat(d3.timeFormat('%d. %b')).tickSize(4).tickSizeOuter(0))
 
     // Create tooltip focus point
     const focusBar = chart.append('g').style('display', 'none')
@@ -148,6 +149,8 @@ svg {
   stroke: $color-primary-subtle;
 }
 /deep/ .line-2 {
+  fill: $color-accent-subtle;
+  fill-opacity: 0.45;
   stroke: $color-accent-subtle;
 }
 /deep/ .overlay {
